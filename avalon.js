f4b3b5646f63613255979a9a8c3f666840bed935
RubyLouvre <cheng19840218@gmail.com>
2013-06-17 21:41:00 +0800

    精简nextTick

--- avalon.js
+++ avalon.js
@@ -208,6 +208,9 @@
             el.removeEventListener(type, fn || noop, !!phase)
         } : function(el, type, fn) {
             el.detachEvent("on" + type, fn || noop)
+        },
+        nextTick: typeof setImmediate === "function" ? setImmediate.bind(window) : function(fn) {
+            setTimeout(fn, 0)
         }
     })
 
@@ -760,78 +763,7 @@
             return false
         }
     }
-    /*********************************************************************
-     *                            UI缓冲处理                     *
-     **********************************************************************/
-    var nextTick
-    if (typeof setImmediate === "function") {
-        // In IE10, Node.js 0.9+, or https://github.com/NobleJS/setImmediate
-        nextTick = setImmediate.bind(window)
-    } else {
-        (function() { //否则用一个链表来维护所有待执行的回调
-            var head = {
-                task: void 0,
-                next: null
-            }
-            var tail = head
-            var maxPendingTicks = 2
-            var pendingTicks = 0
-            var queuedTasks = 0
-            var usedTicks = 0
-            var requestTick = void 0
 
-            function onTick() {
-                --pendingTicks
-                if (++usedTicks >= maxPendingTicks) {
-                    usedTicks = 0
-                    maxPendingTicks *= 4
-                    var expectedTicks = queuedTasks && Math.min(
-                            queuedTasks - 1,
-                            maxPendingTicks)
-                    while (pendingTicks < expectedTicks) {
-                        ++pendingTicks
-                        requestTick()
-                    }
-                }
-
-                while (queuedTasks) {
-                    --queuedTasks
-                    head = head.next
-                    var task = head.task
-                    head.task = void 0
-                    task()
-                }
-
-                usedTicks = 0
-            }
-            nextTick = function(task) {
-                tail = tail.next = {
-                    task: task,
-                    next: null
-                }
-                if (
-                        pendingTicks < ++queuedTasks && pendingTicks < maxPendingTicks) {
-                    ++pendingTicks
-                    requestTick()
-                }
-            }
-            //然后找一个最快响应的异步API来执行这个链表的函数
-            //你可以用postMessage, image.onerror, xhr.onreadychange, MutationObserver 
-            //最差还有个setTimeout 0殿后 http://jsperf.com/postmessage
-            if (typeof MessageChannel !== "undefined") { //管道通信API
-                var channel = new MessageChannel()
-                channel.port1.onmessage = onTick
-                requestTick = function() {
-                    channel.port2.postMessage(0)
-                }
-            } else {
-                requestTick = function() { //IE6-8
-                    setTimeout(onTick, 0)
-                }
-            }
-        })()
-    }
-    avalon.nextTick = nextTick
     /*********************************************************************
      *                           Define                                 *
      **********************************************************************/
@@ -1551,7 +1483,7 @@
                 var id = data.args.join("-"),
                         el = DOC.getElementById(id)
                 if (el && val.$model) { //id为一个设置了type="ms"的script标签
-                    nextTick(function() {
+                    avalon.nextTick(function() {
                         elem.innerHTML = el.text
                         avalon.scan(elem, val)
                     })
