98461b203f540a32731aabaa4c4eb43d7bfbb3ff
qincheng <qincheng@unitedstack.com>
2013-11-01 22:39:18 +0800

    全新rollback机制，解决对象深层监听问题

--- avalon.js
+++ avalon.js
@@ -1113,7 +1113,7 @@
             }
         }
     }
-
+    var updateBack = {}
     function updateViewModel(a, b, valueType) {
         //a为原来的VM， b为新数组或新对象
         if (valueType === "array") {
@@ -1122,79 +1122,21 @@
             a.push.apply(a, bb)
             return a
         } else {
-            var added = [],
-                    removed = [],
-                    updated = [],
-                    astr = [],
-                    bstr = [],
-                    iterators = a[subscribers]
-            var amodel = a.$model
-            //得到要移除的键值对
-            for (var i in amodel) {
-                if (!b || !b.hasOwnProperty(i)) {
-                    removed.push(i)
-                    delete amodel[i]
-                }
-            }
-            // 得到要添加的键值对及其顺序
-            for (var i in b) {
-                if (b.hasOwnProperty(i) && i !== "hasOwnProperty") {
-                    if (!a.hasOwnProperty(i)) {
-                        added.push(i)
-                    }
-                    bstr.push(i)
-                }
-            }
-            //得到其更键的键值对及其顺序
-            for (var i in amodel) {
-                if (amodel.hasOwnProperty(i)) {
-                    astr.push(i)
-                    if (amodel[i] !== b[i]) {
-                        updated.push(i)
-                    }
-                }
-            }
+            var iterators = a[subscribers]
             iterators.forEach(function(fn) {
-                fn("remove", removed)
+              fn.rollback &&  fn.rollback()
             })
-            iterators.forEach(function(fn) {
-                fn("add", b)
-            })
-            if (updated.length) {
-                updated.forEach(function(i) {
-                    var valueType = getType(b[i])
-                    if (rchecktype.test(valueType)) {
-                        updateViewModel(a[i], b[i], valueType)
-                    } else {
-                        a[i] = b[i]
-                    }
-                })
-            }
-            if (astr.join(";") !== bstr.join(";")) {
+            var ret = modelFactory(b)
+            updateBack[ret.$id] = function() {
                 iterators.forEach(function(fn) {
-                    fn("sort", bstr, astr)
+                    try{
+                    var data = fn.data
+                    bindingHandlers[data.type](data, fn.vmodels)
+                    }catch(e){}
                 })
             }
-            var events = a.$events //待到$watch回调都绑定好再移除
-            if (added.length || removed.length) {
-                var scope = a.$model
-                //移除已经删掉的键值对
-                for (var i = 0, name; name = removed[i++]; ) {
-                    delete scope[name]
-                    delete events[name]
-                    delete a.$accessor[name]
-                }
-                for (i = 0, name; name = added[i++]; ) {
-                    scope[name] = b[name]
-                }
-                a = modelFactory(scope, scope, {}, a.$accessor)
-            }
-            a[subscribers] = iterators //替换订阅者列表
-            iterators.forEach(function(fn) {
-                fn.host = a //替换订阅者列表中的视图刷新函数中的宿主（VM）
-            })
-            a.$events = events //替换原先绑定好的$watch回调
-            return a
+            return ret
+
         }
     }
     var isEqual = function(x, y) {
@@ -1205,7 +1147,7 @@
     }
     var unwatchOne = oneObject("$id,$skipArray,$watch,$unwatch,$fire,$events,$model,$accessor," + subscribers)
 
-    function modelFactory(scope, model, watchMore, oldAccessores) {
+    function modelFactory(scope, model, watchMore) {
         if (Array.isArray(scope)) {
             var collection = Collection(scope)
             collection._add(scope)
@@ -1281,7 +1223,13 @@
                                 if (rchecktype.test(valueType)) {
                                     if ("value" in accessor) { //如果已经转换过
                                         value = updateViewModel(value, neo, valueType)
+                                        var fn = updateBack[value.$id]
+                                        accessor.value = value
+                                        if (fn) {
+                                            fn()
+                                        }
                                         vmodel.$fire && vmodel.$fire(name, value, preValue)
+
                                     } else { //如果本来就是VM就直接输出，否则要转换
                                         value = neo.$model ? neo : modelFactory(neo, neo)
                                     }
@@ -1314,11 +1262,6 @@
         }
         for (var i in scope) {
             loop(i, scope[i])
-        }
-        if (oldAccessores) {
-            for (var i in oldAccessores) {
-                accessores[i] = oldAccessores[i]
-            }
         }
         vmodel = defineProperties(vmodel, accessores, VBPublics) //生成一个空的ViewModel
         VBPublics.forEach(function(name) {
@@ -2317,6 +2260,8 @@
                 if (!elem.name) { //如果用户没有写name属性，浏览器默认给它一个空字符串
                     elem.name = generateID()
                 }
+                updateView.data = data
+                updateView.models = vmodels
                 var updateView = modelBinding[tagName](data, array[0], vm)
                 updateView && registerSubscriber(updateView, data)
             }
@@ -2706,10 +2651,12 @@
         if (typeof array === "object") {
             list = array[0].apply(array[0], array[1])
         }
+        data.array = array
         var view = documentFragment.cloneNode(false)
         while (elem.firstChild) {
             view.appendChild(elem.firstChild)
         }
+
         data.template = view
         data.vmodels = vmodels
         if (typeof list !== "object") {
@@ -2730,6 +2677,14 @@
         }
         updateView.element = elem
         updateView.state = data.state
+        updateView.data = data
+        updateView.vmodels = vmodels
+        updateView.rollback = function() {
+            while (elem.firstChild) {
+                elem.removeChild(elem.firstChild)
+            }
+            elem.appendChild(view)
+        }
         updateView.host = list
         list[subscribers] && list[subscribers].push(updateView)
         updateView("add", list, 0)
@@ -2809,15 +2764,15 @@
                 var mapper = withMapper[host.$id] || (withMapper[host.$id] = {})
                 if (!mapper[key]) {
                     val = typeof val === "object" ? modelFactory(val) : val
-                    var proxy = createWithProxy(key, val)
+                    var proxy = createWithProxy(key, val, data.array)
                     mapper[key] = proxy
-                    if (val && val.$model) {
-                        proxy.$events = host.$events
-                        proxy[subscribers] = host[subscribers]
-                    }
-                    host.$watch(key, function(neo) {
-                        proxy.$val = neo
-                    })
+//                    if (val && val.$model) {
+//                        proxy.$events = host.$events
+//                        proxy[subscribers] = host[subscribers]
+//                    }
+//                    host.$watch(key, function(neo) {
+//                        proxy.$val = neo
+//                    })
                 }
                 var tview = data.template.cloneNode(true)
                 scanNodes(tview, [mapper[key], val].concat(data.vmodels), data.state)
@@ -2891,10 +2846,17 @@
     }
     //为子视图创建一个ViewModel
 
-    function createWithProxy(key, val) {
+    function createWithProxy(key, val, array) {
         return modelFactory({
             $key: key,
-            $val: val
+            $val: {
+                get: function() {
+                    try {
+                        return (array[0].apply(array[0], array[1]) || {})[key]
+                    } catch (e) {
+                    }
+                }
+            }
         }, 0, {
             $val: 1
         })
