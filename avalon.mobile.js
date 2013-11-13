986943cb16812a47b4d25daa407f0d342e435f7e
qincheng <qincheng@unitedstack.com>
2013-11-13 16:36:41 +0800

    添加全新的ms-widget绑定

--- avalon.mobile.js
+++ avalon.mobile.js
@@ -1757,23 +1757,27 @@
             bindingHandlers.each(data, vmodels, true)
         },
         "widget": function(data, vmodels) {
-//            还在设计中
-//            var args = data.value.match(rword)
-//            var widget = args[0], ret = 0
-//            if (args.length === 1) {
-//                args.push(widget + setTimeout("1"))
-//            }
-//            data.node.value = args.join(",")
-//            if (typeof avalon.ui[widget] === "function") {
-//                var vmodel = vmodels[0], opts = args[0]
-//                data[ widget + "Option"] = vmodel && opts && typeof vmodel[opts] == "object" ? vmodel[opts] : {}
-//                data[ widget + "id"] = args[1]
-//                avalon.ui[widget](data.element, data, vmodels)
-//                ret = 1
-//            }
-//            data.remove = ret;
+            var args = data.value.match(rword)
+            var widget = args[0], ret = 0
+            if (args.length === 1) {
+                args.push(widget + setTimeout("1"))
+            }
+            data.node.value = args.join(",")
+            var constructor = avalon.ui[widget]
+            if (typeof constructor === "function") {
+                var vmodel = vmodels[0], opts = args[0]//options在最近的一个VM中的名字
+                var vmOptions = vmodel && opts && typeof vmodel[opts] == "object" ? vmodel[opts] : {}
+                var element = avalon.element
+                var elemData = filterData(avalon(element).data(), args[0])//抽取data-tooltip-text、data-tooltip-attr属性，组成一个配置对象
+                data[ widget + "id"] = args[1]
+                data[ widget + "Options"] = avalon.mix({}, constructor.defaults, vmOptions, elemData)
+                constructor(element, data, vmodels)
+                ret = 1
+            }
+            data.remove = ret;
         },
-        ui: function(data, vmodels) {
+        "ui": function(data, vmodels) {
+            log("ms-ui已废弃，请使用更方便的ms-widget")
             var args = data.value.match(rword)
             var elem = data.element, widget = args[0], ret = 0
             if (args.length == 1) {
