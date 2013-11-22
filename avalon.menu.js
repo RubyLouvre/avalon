5b7f786cbf13a8181db86bd35e204cb6339677c7
qincheng <qincheng@unitedstack.com>
2013-11-22 17:12:51 +0800

    update menu

--- avalon.menu.js
+++ avalon.menu.js
@@ -45,28 +45,31 @@ define(["avalon.position"], function(avalon) {
             }
             return array
         }
-        var submenuHTML = '<ul ><li ms-repeat="submenu"   >' +
+        var submenuHTML = '<ul ><li ms-repeat="submenu"   ms-mouseenter="showMenu(el, $index, \'slideRight\')"   ms-mouseleave="hideMenu(el)">' +
                 '<a ms-href="el.href"  ms-class-parent="el.submenu.length" ><span>{{ el.content }}</span></a>' +
-                '<span class="spanbox" ms-if="el.submenu.length" style="">' +
-                '<div ms-include="submenuHTML"></div></span></li></ul>'
+                '<span class="spanbox" ms-if="el.submenu.length"   ms-css-width="width" ms-css-height="height" ms-css-overflow="overflow" ms-css-display="display" ms-css-visibility="visibility" >' +
+                '<div ms-include="submenuHTML" ms-css-overflow="overflow" ms-css-height="height"  ms-css-height="width" ms-css-display="display" ></div></span></li></ul>'
         var script = document.createElement("script")
         script.type = "avalon"
         script.text = submenuHTML
         script.id = "submenuHTML"
         element.parentNode.appendChild(script)
         //  avalon.includeContents
-        var mainMenuHTML = '<ul class="menu"><li ms-repeat-elem="mainmenu"    ms-mouseleave="hideMenu(elem)" ms-class-last="$last" ms-class-active="activeIndex == $index" ms-class-current="currentIndex == $index">' +
-                '<a ms-href="elem.href" ms-mouseenter="showMenu(elem, $index)"  ms-class-parent="elem.submenu.length"><span>{{ elem.content }}</span></a>' +
+        var mainMenuHTML = '<ul class="menu"><li ms-repeat-elem="mainmenu" ms-mouseenter="showMenu(elem, $index,\'slideDown\')"   ms-mouseleave="hideMenu(elem)" ms-class-last="$last" ms-class-active="activeIndex == $index" ms-class-current="currentIndex == $index">' +
+                '<a ms-href="elem.href"  ms-class-parent="elem.submenu.length"><span>{{ elem.content }}</span></a>' +
                 '<span  class="spanbox" ms-if="elem.submenu.length"  ms-css-width="width" ms-css-height="height" ms-css-overflow="overflow" ms-css-display="display" ms-css-visibility="visibility" >' +
-                '<div  ms-if="elem.submenu.length" ms-include="submenuHTML" ms-css-overflow="overflow" ms-css-height="height"  ms-css-display="display" ></div>' +
+                '<div ms-include="submenuHTML"  ms-css-height="height" style="z-index:-1" ms-css-display="display" ></div>' +
                 '</span></li>' +
-                '<li ms-css-left="backLeft" ms-css-width="backWidth" style="overflow: hidden;" class="back"><div class="left"></div></li></ul>'// " ms-css-overflow="overflow"
+                '<li ms-css-left="backLeft" ms-css-width="backWidth"  style="overflow: hidden;" class="back"><div class="left"></div></li></ul>'// " ms-css-overflow="overflow"
         model = avalon.define(data.menuId, function(vm) {
             avalon.mix(vm, options)
             vm.hideMenu = function(scope) {
+               return
                 setTimeout(function() {
                     scope.visibility = "hidden";
                     scope.display = "none"
+                    scope.height = 0
+                    scope.width = 0
                 })
 
             }
@@ -78,40 +81,50 @@ define(["avalon.position"], function(avalon) {
                         return
                     scope.visibility = "visible";
                     scope.display = "block"
-                    var parent = this.parentNode;
-                    parent.style.width = avalon(parent).width() + "px"//必须，防御LI被撑宽
-                    var node = this, spanbox
-                    do {
+                    var effect = arguments[2]
+                    var prop = effect === "slideRight" ? "width" : "height"
+
+                    this.style.width = avalon(this).width() + "px"//必须，防御LI被撑开
+                    this.style.height = avalon(this).height() + "px"
+
+                    var spanbox
+                    for (var i = 0, node; node = this.childNodes[i++]; ) {
                         if (node.nodeType == 1 && /spanbox/.test(node.className)) {
                             spanbox = node
                             break;
                         }
-                    } while ((node = node.nextSibling))
+                    }
                     if (!spanbox._width) {
-                        var ul = parent.getElementsByTagName("ul")[0]
+                        var ul = this.getElementsByTagName("ul")[0]
                         spanbox._width = avalon(ul).width()
                         spanbox._height = avalon(ul).height()
                         spanbox._top = parseFloat(avalon(spanbox).css("top"))
                         spanbox._left = parseFloat(avalon(spanbox).css("left"))
                     }
+                    var div = spanbox.firstElementChild || spanbox.children[0]
                     scope.height = spanbox._height
                     scope.width = spanbox._width
+                    /*
                     scope.overflow = "hidden"
                     var duration = 500
                     var startTime = new Date - 0
-                    var change = spanbox._height 
+                    var change = -1 * spanbox["_" + prop]
+                    var h = parseFloat(span.css("paddingBottom"))
+
+                    div.style.top = change + 'px'
+
                     function animate() {
                         var ellapseTime = new Date - startTime;
                         if (ellapseTime >= duration) {
-                            scope.height = spanbox._height
+                            div.style.top = "0px"
                             scope.overflow = "visible";
                             return;
                         }
-                        scope.height = change * (ellapseTime / duration)
+                        div.style.top = change * (1 - (ellapseTime / duration)) + "px"
                         requestAnimationFrame(animate)
                     }
                     requestAnimationFrame(animate)
-
+*/
                 }
             }
             vm.mainmenu = fix(options.data || [])
