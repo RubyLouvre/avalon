b37314776de612acba716940fc35bb47117d2b57
qincheng <qincheng@unitedstack.com>
2014-01-01 17:34:39 +0800

    update nextTick

--- avalon.js
+++ avalon.js
@@ -302,17 +302,19 @@
     //视浏览器情况采用最快的异步回调
     var BrowserMutationObserver = window.MutationObserver || window.WebKitMutationObserver
     if (BrowserMutationObserver) { //chrome18+, safari6+, firefox14+,ie11+,opera15
+        var now
+        var mutationInput = DOC.createElement("input"), mutationCallbacks = []
+        var observer = new BrowserMutationObserver(function(_, fn) {
+            while (fn = mutationCallbacks.shift()) {
+                fn()
+            }
+        })
+        observer.observe(mutationInput, {
+            attributes: true
+        })
         avalon.nextTick = function(callback) { //2-3ms
-            var input = DOC.createElement("input")
-            var observer = new BrowserMutationObserver(function(mutations) {
-                mutations.forEach(function() {
-                    callback()
-                })
-            })
-            observer.observe(input, {
-                attributes: true
-            })
-            input.setAttribute("value", Math.random())
+            mutationCallbacks.push(callback)
+            mutationInput.setAttribute("value", Math.random())
         }
     } else if (window.VBArray) { //IE下这个通常只要1ms,而且没有副作用，不会发现请求，setImmediate如果只执行一次，与setTimeout一样要140ms上下
         avalon.nextTick = function(callback) {
