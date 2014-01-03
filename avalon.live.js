a0bd4ea31114735b6b0eb0639752067504de63c7
qincheng <qincheng@unitedstack.com>
2014-01-03 23:35:41 +0800

    添加事件代理模块

---
+++ avalon.live.js
@@ -0,0 +1,39 @@
+define(["avalon"], function(avalon) {
+    var DOC = document
+    var root = DOC.documentElement
+    var liveMap = avalon.bindingHandlers.live = function(data, vmodels) {
+        var type = data.param
+        if (!document.createEvent && /focus|blur|change|submit|reset|select/.test(type)) {
+            throw Error("IE6-8不支持" + type + "的事件代理")
+        }
+        if (!liveMap[type]) {
+            liveMap[type] = []
+            avalon.bind(DOC, type, function(e) {
+                var callbacks = liveMap[type]
+                for (var i = callbacks.length, obj; obj = callbacks[--i]; ) {
+                    if (root.contains(obj.elem)) {
+                        obj.fn.call(obj.elem, e)
+                    } else {
+                        callbacks.splice(i, 1)
+                    }
+                }
+            }, true)
+        }
+        data.specialBind = function(elem, fn) {
+            var obj = {
+                elem: elem,
+                fn: fn
+            }
+            var callbacks = liveMap[type]
+            callbacks.unshift(obj)
+            data.specialUnbind = function() {
+                avalon.Array.remove(callbacks, obj)
+                delete data.specialBind
+                delete data.specialUnbind
+            }
+        }
+        avalon.bindingHandlers.on(data, vmodels)
+    }
+
+})
+//avalon的事件代理模块
