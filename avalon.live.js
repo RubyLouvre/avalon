5f1aba88735897d3a8cccd9d1e5307b1ff5248e3
qincheng <qincheng@unitedstack.com>
2014-01-05 13:58:55 +0800

    实现对change事件的代理

--- avalon.live.js
+++ avalon.live.js
@@ -7,27 +7,82 @@ define(["avalon"], function(avalon) {
         "blur": "focusout"
     }
     var checkMap = {}
+
+    function getVal(elem) {
+        var type = elem.type
+        if (type === "select-multiple") {
+            if (elem.selectedIndex > -1) {
+                var ret = []
+                for (var i = 0, el; el = elem.options[i++]; ) {
+                    ret.push(el.selected)
+                }
+                return ret.join("-")
+            } else {
+                return ""
+            }
+
+        } else if (elem.nodeName.toLowerCase() === "select") {
+            return  elem.selectedIndex;
+        }
+
+        return  elem.value
+    }
+    function createEvent(e, type) {
+        var ex = {}
+        for (var j in e) {
+            ex[j] = e[j]
+        }
+        ex.preventDefault = function() { //阻止默认行为
+            e.returnValue = false
+        }
+        ex.stopPropagation = function() { //阻止事件在DOM树中的传播
+            e.cancelBubble = true
+        }
+        ex.originalEvent = e
+        ex.type = type
+        return ex
+    }
+    function testChange(e) {
+        var callbacks = liveMap["fixChangechange"]
+        var target = e.target
+        for (var i = callbacks.length, obj; obj = callbacks[--i]; ) {
+            var elem = obj.elem
+            if (root.contains(elem)) {
+                if (elem === target || elem.contains(target)) {
+                    var curVal = getVal(elem)
+                    if (obj.__change__ !== curVal) {
+                        obj.fn.call(elem, createEvent(e, "change"))
+                        obj.__change__ = curVal
+                    }
+                }
+            } else {
+                callbacks.splice(i, 1)
+            }
+        }
+
+    }
     var liveMap = avalon.bindingHandlers.live = function(data, vmodels) {
         var type = data.param
         var live = "noFix"
         if (!DOC.createEvent) {
-            var fixCheck = type === "change" && /checkbox|radio/.test(data.element.type)
-            if (/focus|blur/.test(type) || fixCheck) {
+            if (/focus|blur/.test(type)) {
                 live = "fixIE"//旧式IE下使用focusin与focusout来模拟focus、blur，使用click来模拟复选框，单选框的change事件
-
+            } else if (type == "change") {
+                var elem = data.element
+                var elemType = elem.type
+                if (elemType == "radio" || elemType === "checkbox") {
+                    live = "fixIE"
+                } else {
+                    live = "fixChange"
+                }
             } else if (/change|submit|reset|select/.test(type)) {
                 live = false//对于一些模拟成本太大的事件直接使用普通的事件绑定
             }
         }
- avalon.bind(DOC, "beforeactivate", function(e) {
-     console.log(e.type+"!!!!!!!")
- })
+
         if (live) {
             if (!liveMap[live + type]) {
                 liveMap[live + type] = []
-                //   console.log(type + "  " + live)
-
-
                 if (live === "noFix") {
                     avalon.bind(DOC, type, function(e) {
                         var callbacks = liveMap[live + type]
@@ -46,7 +101,6 @@ define(["avalon"], function(avalon) {
                 }
 
                 if (live === "fixIE") {
-                    //  var fixType = type === "focus" ? "focusin" : "focusout"
                     avalon.bind(DOC, IEEventMap[type], function(e) {
                         var callbacks = liveMap[live + type]
                         var target = e.target
@@ -55,19 +109,7 @@ define(["avalon"], function(avalon) {
                             if (root.contains(elem)) {
                                 if (elem === target || elem.contains(target)) {
                                     if (type !== "change" || checkMap[elem.name] !== elem) {
-                                        var ex = {}
-                                        for (var j in e) {
-                                            ex[j] = e[j]
-                                        }
-                                        ex.preventDefault = function() { //阻止默认行为
-                                            e.returnValue = false
-                                        }
-                                        ex.stopPropagation = function() { //阻止事件在DOM树中的传播
-                                            e.cancelBubble = true
-                                        }
-                                        ex.originalEvent = e
-                                        ex.type = type
-                                        obj.fn.call(elem, ex)
+                                        obj.fn.call(elem, createEvent(e, type))
                                         if (type === "change") {
                                             checkMap[elem.name] = elem
                                         }
@@ -79,6 +121,10 @@ define(["avalon"], function(avalon) {
                         }
                     })
                 }
+                if (live === "fixChange") {
+                    avalon.bind(DOC, "beforeactivate", testChange)
+                    avalon.bind(DOC, "beforedeactivate", testChange)
+                }
             }
             data.specialBind = function(elem, fn) {
                 var obj = {
@@ -88,8 +134,8 @@ define(["avalon"], function(avalon) {
                 if (/focus|blur/.test(type)) {
                     elem.tabIndex = elem.tabIndex || -1
                 }
-                if (fixCheck) {
-                    obj.__change__ = elem.checked
+                if (live === "fixChange") {
+                    obj.__change__ = getVal(elem)
                 }
                 var callbacks = liveMap[live + type]
                 callbacks.unshift(obj)
