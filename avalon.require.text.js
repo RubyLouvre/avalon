645faaf02c248a71c6b23c9c24da65fca8be6717
qincheng <qincheng@unitedstack.com>
2014-02-16 11:37:48 +0800

    使用HTML Imports实现模板文件的引用

--- avalon.require.text.js
+++ avalon.require.text.js
@@ -1,5 +1,5 @@
 if (window.require && require.config) {
-    require.config.plugins.text = function(url, y, checkDeps) {
+    require.config.plugins.text = function(url, shim) {
         var modules = avalon.modules
         var xhr = new (self.XMLHttpRequest || ActiveXObject)("Microsoft.XMLHTTP")
         var id = url.replace(/[?#].*/, "")
@@ -27,6 +27,39 @@ if (window.require && require.config) {
         xhr.send()
         return id
     }
+    //http://www.html5rocks.com/zh/tutorials/webcomponents/imports/
+    if ('import' in document.createElement('link')) {
+        require.config.plugins.text = function(url, shim) {
+            var modules = avalon.modules
+            var id = url.replace(/[?#].*/, "")
+            modules[id] = {}
+            var link = document.createElement('link')
+            link.rel = 'import'
+            link.href = url
+            link.onload = function() {
+                modules[id].state = 2
+                var content = this.import
+                if (content) {
+                    modules[id].exports = content.documentElement.outerHTML
+                    avalon.require.checkDeps()
+                } else {
+                    avalon.error(url + "对应资源不存在或没有开启 CORS")
+                }
+                setTimeout(function() {
+                    document.head.removeChild(link)
+                })
+            }
+            link.onerror = function() {
+                avalon.error(url + "对应资源不存在或没有开启 CORS")
+                setTimeout(function() {
+                    document.head.removeChild(link)
+                })
+            }
+            document.head.appendChild(link)
+            return id
+        }
+    }
+
 }
 //充许通过AMD进行加载或直接在页面引用，如果是AMD加载，请务必不改文件名（至少文件名保留“avalon.require.text”这些单词）
 if (Object.keys(avalon.modules).join("").indexOf("avalon.require.text") > 0) {
