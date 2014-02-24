6f3fc6f00d1e38cdc22fa7c68eccf9283c86827e
qincheng <qincheng@unitedstack.com>
2014-02-24 16:08:08 +0800

    处理旧式IE模拟HTML5新元素带来的伪标签

--- avalon.js
+++ avalon.js
@@ -1716,6 +1716,9 @@
                 rquote = /^['"]/,
                 rtag = /<\w+\b(?:(["'])[^"]*?(\1)|[^>])*>/i
         var getAttributes = function(elem) {
+            if (elem.outerHTML.slice(0, 2) == "</") {//处理旧式IE模拟HTML5新元素带来的伪标签
+                return []
+            }
             var str = elem.outerHTML.match(rtag)[0]
             var attributes = [],
                     match,
