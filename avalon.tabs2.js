18a8e2f1baafc89cf270562d123914de04b53ceb
qincheng <qincheng@unitedstack.com>
2014-04-01 00:33:01 +0800

    update tabs2重构完毕

--- avalon.tabs2.js
+++ avalon.tabs2.js
@@ -1,9 +1,9 @@
 define(["avalon", "text!avalon.tabs.tab.html", "text!avalon.tabs.panel.html"], function(avalon, tabHTML, panelHTML) {
     var styleEl = document.getElementById("avalonStyle")
     avalon.ui.tabs = function(element, data, vmodels) {
-        var el, tabsParent, tabs = [], tabpanels = []
-        var options = data.tabsOptions
-        var $element = avalon(element)
+        var el, tabsParent, tabs = [], tabpanels = [], options = data.tabsOptions
+
+
         // 清空它内部所有节点，并收集其内容，构建成tabs与tabpanels两个数组
         while (el = element.firstChild) {
             if (el.tagName === "UL" || el.tagName === "OL") {
@@ -28,7 +28,7 @@ define(["avalon", "text!avalon.tabs.tab.html", "text!avalon.tabs.panel.html"], f
         }
 
         var inited = false
-        console.log(options)
+
         var vmodel = avalon.define(data.tabsId, function(vm) {
             avalon.mix(vm, options)
             vm.tabs = []
@@ -39,8 +39,7 @@ define(["avalon", "text!avalon.tabs.tab.html", "text!avalon.tabs.panel.html"], f
                     vm.tabs = tabs
                     vm.tabpanels = tabpanels
                     avalon.nextTick(function() {
-                        $element.addClass("ui-tabs ui-widget ui-widget-content ui-corner-all")
-                        var collapse = options.event !== 'click' ? ' ms-click="collapse($event,$index)"' : ''
+                        avalon(element).addClass("ui-tabs ui-widget ui-widget-content ui-corner-all")
                         // 设置动态模板
                         var tablist = tabHTML.replace("MS_OPTION_EVENT", vmodel.event)
                         //决定是重复利用已有的元素，还是通过ms-include-src引入新内部
@@ -60,36 +59,9 @@ define(["avalon", "text!avalon.tabs.tab.html", "text!avalon.tabs.panel.html"], f
                 }
             }
             vm.$remove = function() {
-                 element.innerHTML = element.textContent = ""
+                element.innerHTML = element.textContent = ""
             }
-            vm.activate = function(e, index) {
-                e.preventDefault()
-                if (vm.tabs[index].disabled === true) {
-                    return
-                }
-                if (vm.event === 'click' && vm.active == index && vm.collapsible) {
-                    vm.collapsed = !vm.collapsed
-                    return
-                }
-                if (vm.collapsible) {
-                    vm.collapsed = false
-                }
-                if (vm.event != index) {
-                    avalon.nextTick(function() {
-                        vm.active = index
-                        options.activate.call(this, e, vmodel)
-                    })
-                }
-            }
-            vm.collapse = function(e, index) {
-                e.preventDefault()
-                if (vm.tabs[index].disabled === true) {
-                    return
-                }
-                if (vm.collapsible) {
-                    vm.collapsed = !vm.collapsed
-                }
-            }
+
             vm.disable = function(index, disable) {
                 disable = typeof disable == "undefined" ? true : disable
                 if (!avalon.isArray(index)) {
@@ -120,44 +92,58 @@ define(["avalon", "text!avalon.tabs.tab.html", "text!avalon.tabs.panel.html"], f
                 vm.tabpanels.push({
                     content: content,
                     contentType: config.contentType
-
                 })
                 vm.tabs.push({
                     title: title,
                     removable: vm.removable,
                     disabled: false
                 })
-
                 if (config.actived) {
                     avalon.nextTick(function() {
-                        vm.active = vm.tabs.length - 1
+                        vmodel.active = vmodel.tabs.length - 1
                     })
-
                 }
             }
-            vm.remove = function(e) {
+            vm.activate = function(e, index) {
                 e.preventDefault()
-                var index = this.$vmodel.$index
                 if (vm.tabs[index].disabled === true) {
                     return
                 }
-                vm.tabs.removeAt(index)
-                vm.tabpanels.removeAt(index)
+                if (vm.event === 'click' && vm.active === index && vm.collapsible) {
+                    vm.active = NaN
+                    return
+                }
+                if (vm.active !== index) {
+                    avalon.nextTick(function() {
+                        vm.active = index
+                        options.activate.call(this, e, vmodel)
+                    })
+                }
+            }
+            vm.remove = function(e, index) {
+                e.preventDefault()
+                e.stopPropagation()
+                if (vmodel.tabs[index].disabled === true) {
+                    return
+                }
+                vmodel.tabs.removeAt(index)
+                vmodel.tabpanels.removeAt(index)
                 index = index > 1 ? index - 1 : 0
                 avalon.nextTick(function() {
-                    vm.active = index
-                    vm.collapsed = false
+                    vmodel.active = index
                 })
+                vm.bottom = options.bottom
             }
-            vm.bottom = options.bottom
         })
         return vmodel
     }
     avalon.ui.tabs.defaults = {
         collapsed: false,
         active: 0, //默认打开第几个面板
-        event: "click", //打开面板的事件，移过(mouseover)还是点击(click)
-        collapsible: false,
+        event: "click", //切换面板的事件，移过(mouseover)还是点击(click)
+        collapsible: false, //当切换面板的事件为click时，
+        //如果对处于激活状态的按钮再点击，将会它失去激活并且对应的面板会收起来
+        //再次点击它时，它还原，并且对应面板重新出现
         bottom: false, //按钮位于上方还是上方
         removable: false, //按钮的左上角是否出现X，用于移除按钮与对应面板
         activate: avalon.noop, // 切换面板后触发的回调
