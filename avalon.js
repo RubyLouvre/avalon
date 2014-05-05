14c4fb4ce953274620707768d50fbf83ef277aa8
vincent <vincentliangws@gmail.com>
2014-05-05 14:34:31 +0800

    fix #360
    判断是否存在getAttributeNode方法，不存在则不扫描节点

--- avalon.js
+++ avalon.js
@@ -1612,22 +1612,25 @@
     function scanTag(elem, vmodels, node) {
         //扫描顺序  ms-skip(0) --> ms-important(1) --> ms-controller(2) --> ms-if(10) --> ms-repeat(100) 
         //--> ms-if-loop(110) --> ms-attr(970) ...--> ms-each(1400)-->ms-with(1500)--〉ms-duplex(2000)垫后
-        var a = elem.getAttribute(prefix + "skip")
-        var b = elem.getAttributeNode(prefix + "important")
-        var c = elem.getAttributeNode(prefix + "controller")
-        if (typeof a === "string") {
-            return
-        } else if (node = b || c) {
-            var newVmodel = VMODELS[node.value]
-            if (!newVmodel) {
+        if (elem.getAttributeNode) { //fix #360
+            var a = elem.getAttribute(prefix + "skip")
+            var b = elem.getAttributeNode(prefix + "important")
+            var c = elem.getAttributeNode(prefix + "controller")
+            if (typeof a === "string") {
                 return
+            } else if (node = b || c) {
+                var newVmodel = VMODELS[node.value]
+                if (!newVmodel) {
+                    return
+                }
+                //ms-important不包含父VM，ms-controller相反
+                vmodels = node === b ? [newVmodel] : [newVmodel].concat(vmodels)
+                elem.removeAttribute(node.name) //removeAttributeNode不会刷新[ms-controller]样式规则
+                avalon(elem).removeClass(node.name) //处理IE6
             }
-            //ms-important不包含父VM，ms-controller相反
-            vmodels = node === b ? [newVmodel] : [newVmodel].concat(vmodels)
-            elem.removeAttribute(node.name) //removeAttributeNode不会刷新[ms-controller]样式规则
-            avalon(elem).removeClass(node.name) //处理IE6
+            scanAttr(elem, vmodels) //扫描特性节点
         }
-        scanAttr(elem, vmodels) //扫描特性节点
+        
     }
 
     function  scanNodes(parent, vmodels) {
