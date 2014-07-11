a6df23b3cbeed2b1a3cb4430e52d0d461dc6460c
Zou Jianqiang <bigqiang@msn.com>
2014-07-11 16:09:55 +0800

    require/define 男足模块的路径，avalon.js已经修复，avalon.mobile.js与之保持一致

--- avalon.mobile.js
+++ avalon.mobile.js
@@ -1345,8 +1345,8 @@
 
 
     function scanTag(elem, vmodels, node) {
-        //扫描顺序  ms-skip(0) --> ms-important(1) --> ms-controller(2) --> ms-if(10) --> ms-repeat(100) 
-        //--> ms-if-loop(110) --> ms-attr(970) ...--> ms-each(1400)-->ms-with(1500)--〉ms-duplex(2000)垫后        
+        //扫描顺序  ms-skip(0) --> ms-important(1) --> ms-controller(2) --> ms-if(10) --> ms-repeat(100)
+        //--> ms-if-loop(110) --> ms-attr(970) ...--> ms-each(1400)-->ms-with(1500)--〉ms-duplex(2000)垫后
         var a = elem.getAttribute(prefix + "skip")
         var b = elem.getAttributeNode(prefix + "important")
         var c = elem.getAttributeNode(prefix + "controller")
@@ -2185,7 +2185,7 @@
             data.handlerName = "attr" //handleName用于处理多种绑定共用同一种bindingExecutor的情况
             parseExprProxy(text, vmodels, data, (simple ? null : scanExpr(data.value)))
         },
-        //根据VM的属性值或表达式的值切换类名，ms-class="xxx yyy zzz:flag" 
+        //根据VM的属性值或表达式的值切换类名，ms-class="xxx yyy zzz:flag"
         //http://www.cnblogs.com/rubylouvre/archive/2012/12/17/2818540.html
         "class": function(data, vmodels) {
             var oldStyle = data.param,
@@ -2200,7 +2200,7 @@
                 var colonIndex = noExpr.indexOf(":") //取得第一个冒号的位置
                 if (colonIndex === -1) { // 比如 ms-class="aaa bbb ccc" 的情况
                     var className = text
-                } else { // 比如 ms-class-1="ui-state-active:checked" 的情况 
+                } else { // 比如 ms-class-1="ui-state-active:checked" 的情况
                     className = text.slice(0, colonIndex)
                     rightExpr = text.slice(colonIndex + 1)
                     parseExpr(rightExpr, vmodels, data) //决定是添加还是删除
@@ -2655,7 +2655,7 @@
             var currHTML = element.innerHTML
             if (currHTML === innerHTML) {
                 clearInterval(id)
-                //先等到select里的option元素被扫描后，才根据model设置selected属性  
+                //先等到select里的option元素被扫描后，才根据model设置selected属性
                 registerSubscriber(data)
             } else {
                 innerHTML = currHTML
@@ -3107,7 +3107,7 @@
         },
         camelize: camelize,
         escape: function(html) {
-            //将字符串经过 html 转义得到适合在页面中显示的内容, 例如替换 < 为 &lt 
+            //将字符串经过 html 转义得到适合在页面中显示的内容, 例如替换 < 为 &lt
             return String(html).
                     replace(/&/g, '&amp;').
                     replace(rsurrogate, function(value) {
@@ -3142,7 +3142,7 @@
                         var k = Math.pow(10, prec)
                         return "" + Math.round(n * k) / k
                     }
-            // Fix for IE parseFloat(0.55).toFixed(0) = 0 
+            // Fix for IE parseFloat(0.55).toFixed(0) = 0
             s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split('.')
             if (s[0].length > 3) {
                 s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
@@ -3177,7 +3177,7 @@
      'a': am/pm marker
      'Z': 4 digit (+sign) representation of the timezone offset (-1200-+1200)
      format string can also be one of the following predefined localizable formats:
-     
+
      'medium': equivalent to 'MMM d, y h:mm:ss a' for en_US locale (e.g. Sep 3, 2010 12:05:08 pm)
      'short': equivalent to 'M/d/yy h:mm a' for en_US locale (e.g. 9/3/10 12:05 pm)
      'fullDate': equivalent to 'EEEE, MMMM d,y' for en_US locale (e.g. Friday, September 3, 2010)
@@ -3605,7 +3605,7 @@
                         ret = ret.replace(rdeuce, "")
                     }
                 } else if (tmp === "/") {
-                    ret = parent + url //相对于兄弟路径
+                    ret = url //相对于根路径
                 } else {
                     avalon.error("不符合模块标识规则: " + url)
                 }
@@ -3853,7 +3853,7 @@
                         touchProxy.fire("swipe" + (swipeDirection(touchProxy.x1, touchProxy.x2, touchProxy.y1, touchProxy.y2)))
                         touchProxy = {}
                     }, 0)
-                    // normal tap 
+                    // normal tap
                 } else if ("last" in touchProxy) {
                     if (deltaX < 30 && deltaY < 30) { //如果移动的距离太小
                         tapTimeout = setTimeout(function() {
