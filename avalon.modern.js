58dabacbe7933d5460abd87e82a17184c9dad424
RubyLouvre <cheng19840218@gmail.com>
2014-08-26 17:38:19 +0800

    让其可以去掉AMD Loader模块

--- avalon.modern.js
+++ avalon.modern.js
@@ -3763,14 +3763,20 @@
         innerRequire.config = kernel
         innerRequire.checkDeps = checkDeps
     }
-    
+
     /*********************************************************************
      *                    DOMReady                                         *
      **********************************************************************/
-
+    var readyList = []
     function fireReady() {
-        modules["ready!"].state = 2
-        innerRequire.checkDeps()
+        if (innerRequire) {
+            modules["ready!"].state = 2
+            innerRequire.checkDeps()//隋性函数，防止IE9二次调用_checkDeps
+        } else {
+            readyList.forEach(function(a) {
+                a(avalon)
+            })
+        }
         fireReady = noop //隋性函数，防止IE9二次调用_checkDeps
     }
 
@@ -3781,7 +3787,13 @@
         window.addEventListener("load", fireReady)
     }
     avalon.ready = function(fn) {
-        innerRequire("ready!", fn)
+        if (innerRequire) {
+            innerRequire("ready!", fn)
+        } else if (fireReady === noop) {
+            fn(avalon)
+        } else {
+            readyList.push(fn)
+        }
     }
     avalon.config({
         loader: true
