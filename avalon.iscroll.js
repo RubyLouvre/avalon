a24993c7d702de466194d3ced486258319411f22
RubyLouvre <cheng19840218@gmail.com>
2014-08-27 20:01:11 +0800

    update iscroll

--- avalon.iscroll.js
+++ avalon.iscroll.js
@@ -321,7 +321,7 @@ define(["avalon"], function() {
         version: '5.1.2',
         _init: function() {
             this._initEvents()
-
+            console.log(this.options.scrollbars || this.options.indicators)
             if (this.options.scrollbars || this.options.indicators) {
                 // this._initIndicators();
             }
@@ -676,57 +676,6 @@ define(["avalon"], function() {
 
             this._execEvent('scrollEnd');
         },
-        scrollBy: function(x, y, time, easing) {
-            x = this.x + x;
-            y = this.y + y;
-            time = time || 0;
-
-            this.scrollTo(x, y, time, easing);
-        },
-        //这个方法接受4个参数 x, y, time, easing x 为移动的x轴坐标，y为移动的y轴坐标, time为移动时间，easing表示使用何种缓动公式。 
-        scrollTo: function(x, y, time, easing) {
-            easing = easing || utils.ease.circular;
-            this.isInTransition = this.options.useTransition && time > 0;// 正在使用CSS3transition
-            if (!time || (this.options.useTransition && easing.style)) {
-                this._transitionTimingFunction(easing.style);
-                this._transitionTime(time);
-                this._translate(x, y);
-            } else {//如果不支持，使用JS动画
-                this._animate(x, y, time, easing.fn);
-            }
-        },
-        // scrollToElement --> scrollTo --> _translate or _animate
-        // scrollBy --> scrollTo --> _translate or _animate
-        scrollToElement: function(el, time, offsetX, offsetY, easing) {
-            el = el.nodeType ? el : this.scroller.querySelector(el);
-
-            if (!el) {
-                return;
-            }
-
-            var pos = utils.offset(el);
-
-            pos.left -= this.wrapperOffset.left;
-            pos.top -= this.wrapperOffset.top;
-
-            // if offsetX/Y are true we center the element to the screen
-            if (offsetX === true) {
-                offsetX = Math.round(el.offsetWidth / 2 - this.wrapper.offsetWidth / 2);
-            }
-            if (offsetY === true) {
-                offsetY = Math.round(el.offsetHeight / 2 - this.wrapper.offsetHeight / 2);
-            }
-
-            pos.left -= offsetX || 0;
-            pos.top -= offsetY || 0;
-
-            pos.left = pos.left > 0 ? 0 : pos.left < this.maxScrollX ? this.maxScrollX : pos.left;
-            pos.top = pos.top > 0 ? 0 : pos.top < this.maxScrollY ? this.maxScrollY : pos.top;
-
-            time = time == null || time === 'auto' ? Math.max(Math.abs(this.x - pos.left), Math.abs(this.y - pos.top)) : time;
-
-            this.scrollTo(pos.left, pos.top, time, easing);
-        },
         _translate: function(x, y) {
             if (this.options.useTransform) {//使用transform
                 this.scrollerStyle[utils.style.transform] = 'translate(' + x + 'px,' + y + 'px)' + this.translateZ;
@@ -846,7 +795,63 @@ define(["avalon"], function() {
         enable: function() {
             this.enabled = true;
         },
-        //===============================处理滚轮事件====================
+        /*********************************************************************
+         *                       滚动到目标位置                        *
+         **********************************************************************/
+        scrollBy: function(x, y, time, easing) {
+            x = this.x + x;
+            y = this.y + y;
+            time = time || 0;
+
+            this.scrollTo(x, y, time, easing);
+        },
+        //这个方法接受4个参数 x, y, time, easing x 为移动的x轴坐标，y为移动的y轴坐标, time为移动时间，easing表示使用何种缓动公式。 
+        scrollTo: function(x, y, time, easing) {
+            easing = easing || utils.ease.circular;
+            this.isInTransition = this.options.useTransition && time > 0;// 正在使用CSS3transition
+            if (!time || (this.options.useTransition && easing.style)) {
+                this._transitionTimingFunction(easing.style);
+                this._transitionTime(time);
+                this._translate(x, y);
+            } else {//如果不支持，使用JS动画
+                this._animate(x, y, time, easing.fn);
+            }
+        },
+        // scrollToElement --> scrollTo --> _translate or _animate
+        // scrollBy --> scrollTo --> _translate or _animate
+        scrollToElement: function(el, time, offsetX, offsetY, easing) {
+            el = el.nodeType ? el : this.scroller.querySelector(el);
+
+            if (!el) {
+                return;
+            }
+
+            var pos = utils.offset(el);
+
+            pos.left -= this.wrapperOffset.left;
+            pos.top -= this.wrapperOffset.top;
+
+            // if offsetX/Y are true we center the element to the screen
+            if (offsetX === true) {
+                offsetX = Math.round(el.offsetWidth / 2 - this.wrapper.offsetWidth / 2);
+            }
+            if (offsetY === true) {
+                offsetY = Math.round(el.offsetHeight / 2 - this.wrapper.offsetHeight / 2);
+            }
+
+            pos.left -= offsetX || 0;
+            pos.top -= offsetY || 0;
+
+            pos.left = pos.left > 0 ? 0 : pos.left < this.maxScrollX ? this.maxScrollX : pos.left;
+            pos.top = pos.top > 0 ? 0 : pos.top < this.maxScrollY ? this.maxScrollY : pos.top;
+
+            time = time == null || time === 'auto' ? Math.max(Math.abs(this.x - pos.left), Math.abs(this.y - pos.top)) : time;
+
+            this.scrollTo(pos.left, pos.top, time, easing);
+        },
+        /*********************************************************************
+         *                       对齐处理                                         *
+         **********************************************************************/
         _initWheel: function() {
             var that = this
             var removeFn = avalon.bind(this.wrapper, "mousewheel", function(e) {
@@ -923,6 +928,251 @@ define(["avalon"], function() {
             this.on('destroy', function() {
                 avalon.bind(this.wrapper, "mousewheel", removeFn)
             });
-        }
+        },
+        /*********************************************************************
+         *                    Snap                                           *
+         **********************************************************************/
+        _initSnap: function() {
+            this.currentPage = {};
+
+            if (typeof this.options.snap == 'string') {
+                this.options.snap = this.scroller.querySelectorAll(this.options.snap);
+            }
+
+            this.on('refresh', function() {
+                var i = 0, l,
+                        m = 0, n,
+                        cx, cy,
+                        x = 0, y,
+                        stepX = this.options.snapStepX || this.wrapperWidth,
+                        stepY = this.options.snapStepY || this.wrapperHeight,
+                        el;
+
+                this.pages = [];
+
+                if (!this.wrapperWidth || !this.wrapperHeight || !this.scrollerWidth || !this.scrollerHeight) {
+                    return;
+                }
+
+                if (this.options.snap === true) {
+                    cx = Math.round(stepX / 2);
+                    cy = Math.round(stepY / 2);
+
+                    while (x > -this.scrollerWidth) {
+                        this.pages[i] = [];
+                        l = 0;
+                        y = 0;
+
+                        while (y > -this.scrollerHeight) {
+                            this.pages[i][l] = {
+                                x: Math.max(x, this.maxScrollX),
+                                y: Math.max(y, this.maxScrollY),
+                                width: stepX,
+                                height: stepY,
+                                cx: x - cx,
+                                cy: y - cy
+                            };
+
+                            y -= stepY;
+                            l++;
+                        }
+
+                        x -= stepX;
+                        i++;
+                    }
+                } else {
+                    el = this.options.snap;
+                    l = el.length;
+                    n = -1;
+
+                    for (; i < l; i++) {
+                        if (i === 0 || el[i].offsetLeft <= el[i - 1].offsetLeft) {
+                            m = 0;
+                            n++;
+                        }
+
+                        if (!this.pages[m]) {
+                            this.pages[m] = [];
+                        }
+
+                        x = Math.max(-el[i].offsetLeft, this.maxScrollX);
+                        y = Math.max(-el[i].offsetTop, this.maxScrollY);
+                        cx = x - Math.round(el[i].offsetWidth / 2);
+                        cy = y - Math.round(el[i].offsetHeight / 2);
+
+                        this.pages[m][n] = {
+                            x: x,
+                            y: y,
+                            width: el[i].offsetWidth,
+                            height: el[i].offsetHeight,
+                            cx: cx,
+                            cy: cy
+                        };
+
+                        if (x > this.maxScrollX) {
+                            m++;
+                        }
+                    }
+                }
+
+                this.goToPage(this.currentPage.pageX || 0, this.currentPage.pageY || 0, 0);
+
+                // Update snap threshold if needed
+                if (this.options.snapThreshold % 1 === 0) {
+                    this.snapThresholdX = this.options.snapThreshold;
+                    this.snapThresholdY = this.options.snapThreshold;
+                } else {
+                    this.snapThresholdX = Math.round(this.pages[this.currentPage.pageX][this.currentPage.pageY].width * this.options.snapThreshold);
+                    this.snapThresholdY = Math.round(this.pages[this.currentPage.pageX][this.currentPage.pageY].height * this.options.snapThreshold);
+                }
+            });
+
+            this.on('flick', function() {
+                var time = this.options.snapSpeed || Math.max(
+                        Math.max(
+                                Math.min(Math.abs(this.x - this.startX), 1000),
+                                Math.min(Math.abs(this.y - this.startY), 1000)
+                                ), 300);
+
+                this.goToPage(
+                        this.currentPage.pageX + this.directionX,
+                        this.currentPage.pageY + this.directionY,
+                        time
+                        );
+            });
+        },
+        _nearestSnap: function(x, y) {
+            if (!this.pages.length) {
+                return {x: 0, y: 0, pageX: 0, pageY: 0};
+            }
+
+            var i = 0,
+                    l = this.pages.length,
+                    m = 0;
+
+            // Check if we exceeded the snap threshold
+            if (Math.abs(x - this.absStartX) < this.snapThresholdX &&
+                    Math.abs(y - this.absStartY) < this.snapThresholdY) {
+                return this.currentPage;
+            }
+
+            if (x > 0) {
+                x = 0;
+            } else if (x < this.maxScrollX) {
+                x = this.maxScrollX;
+            }
+
+            if (y > 0) {
+                y = 0;
+            } else if (y < this.maxScrollY) {
+                y = this.maxScrollY;
+            }
+
+            for (; i < l; i++) {
+                if (x >= this.pages[i][0].cx) {
+                    x = this.pages[i][0].x;
+                    break;
+                }
+            }
+
+            l = this.pages[i].length;
+
+            for (; m < l; m++) {
+                if (y >= this.pages[0][m].cy) {
+                    y = this.pages[0][m].y;
+                    break;
+                }
+            }
+
+            if (i == this.currentPage.pageX) {
+                i += this.directionX;
+
+                if (i < 0) {
+                    i = 0;
+                } else if (i >= this.pages.length) {
+                    i = this.pages.length - 1;
+                }
+
+                x = this.pages[i][0].x;
+            }
+
+            if (m == this.currentPage.pageY) {
+                m += this.directionY;
+
+                if (m < 0) {
+                    m = 0;
+                } else if (m >= this.pages[0].length) {
+                    m = this.pages[0].length - 1;
+                }
+
+                y = this.pages[0][m].y;
+            }
+
+            return {
+                x: x,
+                y: y,
+                pageX: i,
+                pageY: m
+            };
+        },
+        goToPage: function(x, y, time, easing) {
+            easing = easing || this.options.bounceEasing;
+
+            if (x >= this.pages.length) {
+                x = this.pages.length - 1;
+            } else if (x < 0) {
+                x = 0;
+            }
+
+            if (y >= this.pages[x].length) {
+                y = this.pages[x].length - 1;
+            } else if (y < 0) {
+                y = 0;
+            }
+
+            var posX = this.pages[x][y].x,
+                    posY = this.pages[x][y].y;
+
+            time = time === undefined ? this.options.snapSpeed || Math.max(
+                    Math.max(
+                            Math.min(Math.abs(posX - this.x), 1000),
+                            Math.min(Math.abs(posY - this.y), 1000)
+                            ), 300) : time;
+
+            this.currentPage = {
+                x: posX,
+                y: posY,
+                pageX: x,
+                pageY: y
+            };
+
+            this.scrollTo(posX, posY, time, easing);
+        },
+        next: function(time, easing) {
+            var x = this.currentPage.pageX,
+                    y = this.currentPage.pageY;
+
+            x++;
+
+            if (x >= this.pages.length && this.hasVerticalScroll) {
+                x = 0;
+                y++;
+            }
+
+            this.goToPage(x, y, time, easing);
+        },
+        prev: function(time, easing) {
+            var x = this.currentPage.pageX,
+                    y = this.currentPage.pageY;
+
+            x--;
+
+            if (x < 0 && this.hasVerticalScroll) {
+                x = 0;
+                y--;
+            }
+
+            this.goToPage(x, y, time, easing);
+        },
     }
 })
