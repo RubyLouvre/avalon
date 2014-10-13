e7a1e5a6c00857caed2ba7446391497936ea9ccd
qincheng <qincheng@unitedstack.com>
2014-10-13 23:53:17 +0800

    添加有用注释

--- avalon.js
+++ avalon.js
@@ -902,7 +902,7 @@
      **********************************************************************/
 
     function fixContains(root, el) {
-        try {
+        try {//IE6-8,游离于DOM树外的文本节点，访问parentNode有时会抛错
             while ((el = el.parentNode))
                 if (el === root)
                     return true;
