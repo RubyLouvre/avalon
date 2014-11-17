b375083d9628c6af7b07eb66116e54d358ed68d7
qincheng <qincheng@unitedstack.com>
2014-11-18 02:05:43 +0800

    修复IE6-8下动态生成tr元素时，浏览器私自插入caption元素，导致locateFragment方法计算出错的BUG fix https://github.com/RubyLouvre/avalon/issues/572

--- examples/avalon.shim.js
+++ examples/avalon.shim.js
@@ -1694,7 +1694,7 @@
         for (i = wrap[0]; i--; wrapper = wrapper.lastChild) {
         }
         if (!W3C) { //fix IE
-            for (els = wrapper["getElementsByTagName"]("br"), i = 0; el = els[i++]; ) {
+            for (els = avalon.slice(wrapper["getElementsByTagName"]("br")), i = 0; el = els[i++]; ) {
                 if (el.className && el.className === "msNoScope") {
                     el.parentNode.removeChild(el)
                 }
@@ -1702,6 +1702,14 @@
             for (els = wrapper.all, i = 0; el = els[i++]; ) { //fix VML
                 if (isVML(el)) {
                     fixVML(el)
+                }
+            }
+            if (tag === "tr") {
+                for (els = avalon.slice(wrapper.children), i = 0; el = els[i++]; ) {
+                    // IE6-8,如果动态生成tr元素，必须会在后面添加早已废弃caption的标签，其nodeName,innerHTML都为""
+                    if (el.nodeName == "") {
+                        el.parentNode.removeChild(el)
+                    }
                 }
             }
         }
