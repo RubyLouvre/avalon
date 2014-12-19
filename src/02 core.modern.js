f850747e2ebb1cf4fbcbb38c3f5bfd056122daa9
RubyLouvre <cheng19840218@gmail.com>
2014-12-19 19:34:28 +0800

    fix core version

--- src/02 core.modern.js
+++ src/02 core.modern.js
@@ -100,7 +100,7 @@ function _number(a, len) { //用于模拟slice, splice的效果
 avalon.mix({
     rword: rword,
     subscribers: subscribers,
-    version: 1.37,
+    version: 1.38,
     ui: {},
     log: log,
     slice: function(nodes, start, end) {
