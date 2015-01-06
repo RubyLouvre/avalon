32e71018d18c30ac6caf84439c0cfef4dee5ee23
RubyLouvre <cheng19840218@gmail.com>
2015-01-06 14:24:19 +0800

    update  avalon.shim

--- min/avalon.shim.js
+++ min/avalon.shim.js
@@ -4660,17 +4660,28 @@ new function() {
 /*********************************************************************
  *                     END                                  *
  **********************************************************************/
-avalon.ready = noop
-if (W3C) {
-    DOC.addEventListener("DOMContentLoaded", function() {
-        avalon.scan(DOC.body)
+new function() {
+    avalon.config({
+        loader: false
     })
-} else {
-    avalon.bind(window, "load", function() {
+    var fns = [], listener,
+            hack = root.doScroll,
+            domContentLoaded = 'DOMContentLoaded',
+            loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(DOC.readyState)
+    if (!loaded) {
+        DOC.addEventListener(domContentLoaded, listener = function() {
+            DOC.removeEventListener(domContentLoaded, listener)
+            loaded = 1
+            while (listener = fns.shift())
+                listener()
+        })
+    }
+    avalon.ready = function(fn) {
+        loaded ? fn() : fns.push(fn)
+    }
+    avalon.ready(function() {
         avalon.scan(DOC.body)
     })
 }
-avalon.config({
-    loader: false
-})
+
 })();
