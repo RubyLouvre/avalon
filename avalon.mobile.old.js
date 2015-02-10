e7a5a7f2079725a634b6db2f84814521514866ad
RubyLouvre <cheng19840218@gmail.com>
2015-02-10 15:24:43 +0800

    update touch

--- avalon.mobile.old.js
+++ avalon.mobile.old.js
@@ -5,8 +5,8 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon.mobile.old.js(安卓2.3及其他旧的移动端浏览器) 1.391 build in 2015.2.9 
-d other browsers
+ avalon.mobile.old.js(安卓2.3及其他旧的移动端浏览器) 1.391 build in 2015.2.10 
+ other browsers
  ==================================================*/
 (function(global, factory) {
 
--- avalon.modern.js
+++ avalon.modern.js
@@ -5,8 +5,8 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon.modern.js 1.391 build in 2015.2.9 
-______________________________
+ avalon.modern.js 1.391 build in 2015.2.10 
+_____________________________
  support IE6+ and other browsers
  ==================================================*/
 (function(global, factory) {
