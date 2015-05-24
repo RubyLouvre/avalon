58f47266d50fc4a07f1488cc202f0dce3f2d58db
qincheng <qincheng@unitedstack.com>
2015-05-25 01:24:25 +0800

    优化scanAttr

--- src/12 scanAttr.js
+++ src/12 scanAttr.js
@@ -1,56 +1,56 @@
-function scanAttr(elem, vmodels) {
-    //防止setAttribute, removeAttribute时 attributes自动被同步,导致for循环出错
-    var attributes = getAttributes ? getAttributes(elem) : elem.attributes
-    var bindings = [],
-            msData = {},
-            match
-    var fixAttrs = []
-    for (var i = 0, attr; attr = attributes[i++]; ) {
-        if (attr.specified) {
-            if (match = attr.name.match(rmsAttr)) {
-                //如果是以指定前缀命名的
-                var type = match[1]
-                var param = match[2] || ""
-                var value = attr.value
-                var name = attr.name
-                if (events[type]) {
-                    param = type
-                    type = "on"
-                } else if (obsoleteAttrs[type]) {
-                    log("warning!请改用ms-attr-" + type + "代替ms-" + type + "！")
-                    if (type === "enabled") {//吃掉ms-enabled绑定,用ms-disabled代替
-                        log("warning!ms-enabled或ms-attr-enabled已经被废弃")
-                        type = "disabled"
-                        value = "!(" + value + ")"
+function scanAttr(elem, vmodels, match) {
+    var scanNode = true
+    if (vmodels.length) {
+        var attributes = getAttributes ? getAttributes(elem) : elem.attributes
+        var bindings = []
+        var fixAttrs = []
+        var msData = {}
+        for (var i = 0, attr; attr = attributes[i++]; ) {
+            if (attr.specified) {
+                if (match = attr.name.match(rmsAttr)) {
+                    //如果是以指定前缀命名的
+                    var type = match[1]
+                    var param = match[2] || ""
+                    var value = attr.value
+                    var name = attr.name
+                    if (events[type]) {
+                        param = type
+                        type = "on"
+                    } else if (obsoleteAttrs[type]) {
+                        log("warning!请改用ms-attr-" + type + "代替ms-" + type + "！")
+                        if (type === "enabled") {//吃掉ms-enabled绑定,用ms-disabled代替
+                            log("warning!ms-enabled或ms-attr-enabled已经被废弃")
+                            type = "disabled"
+                            value = "!(" + value + ")"
+                        }
+                        param = type
+                        type = "attr"
+                        name = "ms-" + type + param
+                        fixAttrs.push([attr.name, name, value])
                     }
-                    param = type
-                    type = "attr"
-                    name = "ms-" + type + param
-                    fixAttrs.push([attr.name, name, value])
-                }
-                msData[name] = value
-                if (typeof bindingHandlers[type] === "function") {
-                    var binding = {
-                        type: type,
-                        param: param,
-                        element: elem,
-                        name: name,
-                        value: value,
-                        priority: type in priorityMap ? priorityMap[type] : type.charCodeAt(0) * 10 + (Number(param) || 0)
-                    }
-                    if (type === "html" || type === "text") {
-                        var token = getToken(value)
-                        avalon.mix(binding, token)
-                        binding.filters = binding.filters.replace(rhasHtml, function () {
-                            binding.type = "html"
-                            binding.group = 1
-                            return ""
-                        })// jshint ignore:line
-                    }
-                    if (name === "ms-if-loop") {
-                        binding.priority += 100
-                    }
-                    if (vmodels.length) {
+                    msData[name] = value
+                    if (typeof bindingHandlers[type] === "function") {
+                        var binding = {
+                            type: type,
+                            param: param,
+                            element: elem,
+                            name: name,
+                            value: value,
+                            priority: type in priorityMap ? priorityMap[type] : type.charCodeAt(0) * 10 + (Number(param) || 0)
+                        }
+                        if (type === "html" || type === "text") {
+                            var token = getToken(value)
+                            avalon.mix(binding, token)
+                            binding.filters = binding.filters.replace(rhasHtml, function () {
+                                binding.type = "html"
+                                binding.group = 1
+                                return ""
+                            })// jshint ignore:line
+                        } else if (type === "duplex") {
+                            var hasDuplex = name
+                        } else if (name === "ms-if-loop") {
+                            binding.priority += 100
+                        }
                         bindings.push(binding)
                         if (type === "widget") {
                             elem.msData = elem.msData || msData
@@ -59,34 +59,32 @@ function scanAttr(elem, vmodels) {
                 }
             }
         }
-    }
-    if (bindings.length) {
-        bindings.sort(bindingSorter)
-        fixAttrs.forEach(function (arr) {
-            elem.removeAttribute(arr[0])
-            elem.setAttribute(arr[1], arr[2])
-        })
-        //http://bugs.jquery.com/ticket/7071
-        //在IE下对VML读取type属性,会让此元素所有属性都变成<Failed>
-        if (("form" in elem) && msData["ms-duplex"]) {
-            var control = elem.type
-            if (msData["ms-attr-checked"] && /radio|checkbox/.test(control)) {
-                log("warning!" + control + "控件不能同时定义ms-attr-checked与ms-duplex")
+        if (bindings.length) {
+            bindings.sort(bindingSorter)
+            fixAttrs.forEach(function (arr) {
+                elem.removeAttribute(arr[0])
+                elem.setAttribute(arr[1], arr[2])
+            })
+            //http://bugs.jquery.com/ticket/7071
+            //在IE下对VML读取type属性,会让此元素所有属性都变成<Failed>
+            if (hasDuplex) {
+                if (msData["ms-attr-checked"]) {
+                    log("warning!一个控件不能同时定义ms-attr-checked与" + hasDuplex)
+                }
+                if (msData["ms-attr-value"]) {
+                    log("warning!一个控件不能同时定义ms-attr-value与" + hasDuplex)
+                }
             }
-            if (msData["ms-attr-value"] && /text|password/.test(control)) {
-                log("warning!" + control + "控件不能同时定义ms-attr-value与ms-duplex")
+            for (i = 0; binding = bindings[i]; i++) {
+                type = binding.type
+                if (rnoscanAttrBinding.test(type)) {
+                    return executeBindings(bindings.slice(0, i + 1), vmodels)
+                } else if (scanNode) {
+                    scanNode = !rnoscanNodeBinding.test(type)
+                }
             }
+            executeBindings(bindings, vmodels)
         }
-        var scanNode = true
-        for (i = 0; binding = bindings[i]; i++) {
-            type = binding.type
-            if (rnoscanAttrBinding.test(type)) {
-                return executeBindings(bindings.slice(0, i + 1), vmodels)
-            } else if (scanNode) {
-                scanNode = !rnoscanNodeBinding.test(type)
-            }
-        }
-        executeBindings(bindings, vmodels)
     }
     if (scanNode && !stopScan[elem.tagName] && rbind.test(elem.innerHTML.replace(rlt, "<").replace(rgt, ">"))) {
         mergeTextNodes && mergeTextNodes(elem)
--- src/12 scanAttr.next.js
+++ src/12 scanAttr.next.js
@@ -1,43 +1,44 @@
-function scanAttr(elem, vmodels) {
-    //防止setAttribute, removeAttribute时 attributes自动被同步,导致for循环出错
-    var attributes = elem.attributes
-    var bindings = [],
-            msData = {},
-            match
-    for (var i = 0, attr; attr = attributes[i++]; ) {
-        if (match = attr.name.match(rmsAttr)) {
-            //如果是以指定前缀命名的
-            var type = match[1]
-            var param = match[2] || ""
-            var value = attr.value
-            var name = attr.name
-            msData[name] = value
-            if (events[type]) {
-                param = type
-                type = "on"
-            }
-            if (typeof bindingHandlers[type] === "function") {
-                var binding = {
-                    type: type,
-                    param: param,
-                    element: elem,
-                    name: name,
-                    value: value,
-                    priority: type in priorityMap ? priorityMap[type] : type.charCodeAt(0) * 10 + (Number(param) || 0)
+function scanAttr(elem, vmodels, match) {
+    var scanNode = true
+    if (vmodels.length) {
+        var attributes = elem.attributes
+        var bindings = []
+        var msData = {}
+        for (var i = 0, attr; attr = attributes[i++]; ) {
+            if (match = attr.name.match(rmsAttr)) {
+                //如果是以指定前缀命名的
+                var type = match[1]
+                var param = match[2] || ""
+                var value = attr.value
+                var name = attr.name
+                msData[name] = value
+                if (events[type]) {
+                    param = type
+                    type = "on"
                 }
-                if (type === "html" || type === "text") {
-                    var token = getToken(value)
-                    avalon.mix(binding, token)
-                    binding.filters = binding.filters.replace(rhasHtml, function () {
-                        binding.type = "html"
-                        binding.group = 1
-                        return ""
-                    })
-                }
-                if (name === "ms-if-loop") {
-                    binding.priority += 100
-                }
-                if (vmodels.length) {
+                if (typeof bindingHandlers[type] === "function") {
+                    var binding = {
+                        type: type,
+                        param: param,
+                        element: elem,
+                        name: name,
+                        value: value,
+                        priority: type in priorityMap ? priorityMap[type] : type.charCodeAt(0) * 10 + (Number(param) || 0)
+                    }
+                    if (type === "html" || type === "text") {
+                        var token = getToken(value)
+                        avalon.mix(binding, token)
+                        binding.filters = binding.filters.replace(rhasHtml, function () {
+                            binding.type = "html"
+                            binding.group = 1
+                            return ""
+                        })
+                    } else if (type === "duplex") {
+                        var hasDuplex = name
+                    } else if (name === "ms-if-loop") {
+                        binding.priority += 100
+                    }
+
                     bindings.push(binding)
                     if (type === "widget") {
                         elem.msData = elem.msData || msData
@@ -49,15 +50,14 @@ function scanAttr(elem, vmodels) {
     if (bindings.length) {
         bindings.sort(bindingSorter)
         var control = elem.type
-        if (control && msData["ms-duplex"]) {
-            if (msData["ms-attr-checked"] && /radio|checkbox/.test(control)) {
-                log("warning!" + control + "控件不能同时定义ms-attr-checked与ms-duplex")
+        if (control && hasDuplex) {
+            if (msData["ms-attr-checked"]) {
+                log("warning!" + control + "控件不能同时定义ms-attr-checked与" + hasDuplex)
             }
-            if (msData["ms-attr-value"] && /text|password/.test(control)) {
-                log("warning!" + control + "控件不能同时定义ms-attr-value与ms-duplex")
+            if (msData["ms-attr-value"]) {
+                log("warning!" + control + "控件不能同时定义ms-attr-value与" + hasDuplex)
             }
         }
-        var scanNode = true
         for (var i = 0, binding; binding = bindings[i]; i++) {
             var type = binding.type
             if (rnoscanAttrBinding.test(type)) {
