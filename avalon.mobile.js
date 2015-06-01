9a1c3fa495b40aae078982d89bccfffb25f3b4ba
qincheng <qincheng@unitedstack.com>
2015-06-01 22:16:59 +0800

    fix removeAll BUG

--- avalon.mobile.js
+++ avalon.mobile.js
@@ -1278,9 +1278,12 @@ var arrayPrototype = {
     },
     removeAll: function (all) { //移除N个元素
         if (Array.isArray(all)) {
-            all.forEach(function (el) {
-                this.remove(el)
-            }, this)
+             for (var i = this.length - 1; i >= 0; i--) {
+                var el = this[i]
+                if (all.indexOf(el) !== -1) {
+                    this.removeAt(i)
+                }
+            }
         } else if (typeof all === "function") {
             for (var i = this.length - 1; i >= 0; i--) {
                 var el = this[i]
