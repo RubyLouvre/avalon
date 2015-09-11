13fb2de91eae78b0774b829cdf427b71a36a352c
Rocky Zhang (rockyzh) <rockyzh@gmail.com>
2015-09-11 15:09:03 +0800

    修改loader不能正确处理url path map之后带query的bug

--- avalon.js
+++ avalon.js
@@ -5596,11 +5596,11 @@ new function () {// jshint ignore:line
         //5. 还原扩展名，query
         var urlNoQuery = url + ext
         url = urlNoQuery + this.query
-/*        urlNoQuery = url.replace(rquery, function (a) {
+        urlNoQuery = url.replace(rquery, function (a) {
             this.query = a
             return ""
         })
-*/        //6. 处理urlArgs
+        //6. 处理urlArgs
         eachIndexArray(id, kernel.urlArgs, function (value) {
             url += (url.indexOf("?") === -1 ? "?" : "&") + value;
         })
