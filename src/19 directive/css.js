9c7d7e849af8af55519b5935d956229f96b42268
RubyLouvre <1669866773@qq.com>
2015-12-30 22:03:07 +0800

    优化ms-css

--- src/19 directive/css.js
+++ src/19 directive/css.js
@@ -2,19 +2,17 @@ avalon.directive("css", {
     init: directives.attr.init,
     change: function (val, binding) {
         var elem = binding.element
-        if (elem) {
-            var change = addData(elem, "changeCss")
-            change[this.param] = val
-            addHooks(this, binding)
-        }
+        if (!elem || elem.disposed)
+            return
+        var change = addData(elem, "changeCss")
+        change[this.param] = val
+        addHooks(this, binding)
     },
     update: function (elem, vnode) {
         var change = vnode.changeCss
         var wrap = avalon(elem)
         for (var name in change) {
-            if (name !== "display") {
-                wrap.css(name, change[name])
-            }
+            wrap.css(name, change[name])
         }
         delete vnode.changeCss
     }
