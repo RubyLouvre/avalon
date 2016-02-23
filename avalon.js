61b29b2dc30dca84803a9580161d5b11dffcb944
RubyLouvre <1669866773@qq.com>
2016-02-23 17:48:45 +0800

    修复 1.56 双重循环同名属性报错

--- avalon.js
+++ avalon.js
@@ -5,7 +5,7 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon.js 1.5.6 built in 2016.2.21
+ avalon.js 1.5.6 built in 2016.2.23
  support IE6+ and other browsers
  ==================================================*/
 (function(global, factory) {
