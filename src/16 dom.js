65154883a10184e6dd8d294b207a86e9f9d3b3b6
charlie <xiarugu@163.com>
2016-03-16 16:41:37 +0800

    修复前置空格导致ms-class失效的问题

--- src/16 dom.js
+++ src/16 dom.js
@@ -81,7 +81,7 @@ avalon.fn.mix({
     },
     toggleClass: function (value, stateVal) {
         var className, i = 0
-        var classNames = String(value).split(/\S+/g) || []
+        var classNames = String(value).match(/\S+/g) || []
         var isBool = typeof stateVal === "boolean"
         while ((className = classNames[i++])) {
             var state = isBool ? stateVal : !this.hasClass(className)
