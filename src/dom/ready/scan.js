d6e9b668af8da4b843682a06d660ae2bef12b291
RubyLouvre <cheng19840218@gmail.com>
2016-03-24 01:59:24 +0800

    优化scan的预处理

--- src/dom/ready/scan.js
+++ src/dom/ready/scan.js
@@ -4,9 +4,9 @@ function scan(nodes) {
             var $id = hasController(elem)
             var vm = avalon.vmodels[$id]
             if (vm && !vm.$element) {
+                cleanWhitespace(elem)//减少虚拟DOM的规模及diff, patch的时间
                 var str = elem.outerHTML
                 avalon(elem).removeClass('ms-controller')
-
                 vm.$element = elem
                 var now = new Date() - 0
                 var vtree = avalon.lexer(str)
@@ -24,7 +24,23 @@ function scan(nodes) {
         }
     }
 }
-
+var notWhitespace = /\S/
+function cleanWhitespace(target) {
+    var keep
+    for (var i = 0; i < target.childNodes.length; i++) {
+        var node = target.childNodes[i]
+        if ((node.nodeType == 3) && (!notWhitespace.test(node.nodeValue))) {
+            keep = target.removeChild(node)
+            i--
+        }
+        if (node.nodeType === 1) {
+            cleanWhitespace(node)
+        }
+    }
+    if (target.childNodes.length == 0 && keep) {
+        target.appendChild(keep)
+    }
+}
 module.exports = avalon.scan = function (a) {
     if (!a || !a.nodeType) {
         avalon.warn('[avalon.scan] first argument must be element , documentFragment, or document')
--- src/seed/lang.compact.js
+++ src/seed/lang.compact.js
@@ -16,7 +16,7 @@ var meta = {
     '\\': '\\\\'
 }
 var quote = typeof JSON !== 'undefined' ? JSON.stringify : function (str) {
-    return '"' + str.replace(/[\\\'\x00-\x1f]/g, function (a) {
+    return '"' + String(str).replace(/[\\\'\x00-\x1f]/g, function (a) {
         var c = meta[a];
         return typeof c === 'string' ? c :
                 '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
