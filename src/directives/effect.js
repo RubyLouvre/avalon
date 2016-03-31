a22da4c344fead6802b16560bc196b84cf0cfd5a
RubyLouvre <cheng19840218@gmail.com>
2016-04-01 01:40:41 +0800

    add

--- src/directives/effect.js
+++ src/directives/effect.js
@@ -24,46 +24,33 @@ avalon.directive("effect", {
         if (!type || !options || typeof effect[method] !== 'function')
             return
 
-
-        effect[method](dom, options)
-
-
-
-
-        var inlineStyles = elem.style
-        var computedStyles = window.getComputedStyle ? window.getComputedStyle(elem) : null
-        var useAni = false
-        if (computedStyles && (supportTransition || supportAnimation)) {
-
-            //如果支持CSS动画
-            var duration = inlineStyles[transitionDuration] || computedStyles[transitionDuration]
-            if (duration && duration !== '0s') {
-                elem.setAttribute("data-effect-driver", "t")
-                useAni = true
+        if (options.queue) {
+            if (animationQueue.length) {
+                animationQueue.push(function () {
+                    effect[method](dom, options)
+                })
+            } else {
+                effect[method](dom, options)
             }
-
-            if (!useAni) {
-
-                duration = inlineStyles[animationDuration] || computedStyles[animationDuration]
-                if (duration && duration !== '0s') {
-                    elem.setAttribute("data-effect-driver", "a")
-                    useAni = true
-                }
-
-            }
-        }
-
-        if (!useAni) {
-            if (avalon.effects[name]) {
-                elem.setAttribute("data-effect-driver", "j")
-                useAni = true
-            }
-        }
-        if (useAni) {
-            elem.setAttribute("data-effect-name", name)
+        } else {
+            effect[method](dom, options)
         }
     }
 })
+var animationQueue = []
+var lock = false
+function callNextAniation() {
+    if (lock)
+        return
+    lock = true
+    var fn = animationQueue.shift()
+    if (fn) {
+        avalon.nextTick(function () {
+            lock = false
+            fn()
+        })
+    }
+}
 
 avalon.effects = {}
 avalon.effect = function (name, define) {
@@ -87,12 +74,75 @@ avalon.effect = function (name, define) {
         obj.dir = 'enter'
     }
     //js 则需要
-    var fn = obj.enter || obj.leave
-    if (typeof fn === 'functon') {
+}
 
+
+var Effect = function (el) {
+    this.el = el
+}
+avalon.Effect = Effect
+Effect.prototype = {
+    enter: createMethod('enter'),
+    leave: createMethod('leave'),
+    move: createMethod('move')
+}
+
+function createMethod(action) {
+    var lower = action.toLowerCase()
+    return  function (options) {
+        function wrapDone(e) {
+            var isOk = e !== false
+            var dirWord = isOk ? 'Done' : 'Abort'
+            var callback = options['on' + action + dirWord]
+            callback = callback || noop
+            callback.call(elem, {
+                type: action + dirWord.toLowerCase(),
+                target: elem
+            })
+            el.unbind(transitionEndEvent)
+            el.unbind(animationEndEvent)
+            el.removeClass(options[lower + 'ActiveClass'])
+            callNextAniation()
+        }
+        var elem = this.el
+        var onBeforeEnter = options['onBefore' + action]
+        if (onBeforeEnter) {
+            onBeforeEnter.call(elem, {type: 'before' + lower, target: elem})
+        }
+        if (options[lower]) {
+            options[lower](elem, function (ok) {
+                wrapDone(!!ok)
+            })
+        } else if (supportCssAnimation) {
+            var el = avalon(elem)
+            el.addClass(options[lower + 'Class'])
+            function wrapDone(e) {
+                var isOk = e !== false
+                var dirWord = isOk ? 'Done' : 'Abort'
+                var callback = options['on' + action + dirWord]
+                callback = callback || noop
+                callback.call(elem, {
+                    type: action + dirWord.toLowerCase(),
+                    target: elem
+                })
+                el.unbind(transitionEndEvent)
+                el.unbind(animationEndEvent)
+                el.removeClass(options[lower + 'ActiveClass'] || '')
+                callNextAniation()
+            }
+            el.bind(transitionEndEvent, wrapDone)
+            el.bind(animationEndEvent, wrapDone)
+            avalon.nextTick(function () {
+                el.addClass(options[lower + 'ActiveClass'])
+                var el = window.getComputedStyle(elem)
+                var tranDuration = computedStyles[transitionDuration]
+                var animDuration = computedStyles[animationDuration]
+                if (tranDuration == '0s' && animDuration === '0s') {
+                    wrapDone(false)
+                }
+            })
+        }
     }
-
-
 }
 
 
@@ -160,190 +210,7 @@ new function () {// jshint ignore:line
 
 }()
 
-var Effect = function (el) {
-    this.el = el
-}
-avalon.Effect = Effect
-Effect.prototype = {
-    enter: function (options) {
-        if(options.enter){
-            options.enter(this.el, function(){
-                
-            })
-            
-        }else if (supportCssAnimation) {
-            var el = avalon(this.el)
-            el.addClass(options.enterClass)
-            el.bind(transitionEndEvent, function (e) {
-                options.enterDone.call(el[0], e)
-                el.unbind(transitionEndEvent)
-                el.removeClass(options.enterActiveClass)
-            })
-            el.bind(animationEndEvent, function (e) {
-                options.enterDone.call(el[0], e)
-                el.unbind(animationEndEvent)
-                el.removeClass(options.enterActiveClass)
-            })
-            avalon.nextTick(function () {
-                el.addClass(options.enterActiveClass)
-            })
-        }
-    }
-}
 
-
-
-
-
-var effectPool = []//重复利用动画实例
-function effectFactory(el, opts) {
-    if (!el || el.nodeType !== 1) {
-        return null
-    }
-    if (opts) {
-        var name = opts.effectName
-        var driver = opts.effectDriver
-    } else {
-        name = el.getAttribute("data-effect-name")
-        driver = el.getAttribute("data-effect-driver")
-    }
-    if (!name || !driver) {
-        return null
-    }
-
-    var instance = effectPool.pop() || new Effect()
-    instance.el = el
-    instance.driver = driver
-    instance.useCss = driver !== "j"
-    if (instance.useCss) {
-        opts && avalon(el).addClass(opts.effectClass)
-        instance.cssEvent = driver === "t" ? transitionEndEvent : animationEndEvent
-    }
-    instance.name = name
-    instance.callbacks = avalon.effects[name] || {}
-
-    return instance
-
-
-}
-
-function effectBinding(elem, binding) {
-    var name = elem.getAttribute("data-effect-name")
-    if (name) {
-        binding.effectName = name
-        binding.effectDriver = elem.getAttribute("data-effect-driver")
-        var stagger = +elem.getAttribute("data-effect-stagger")
-        binding.effectLeaveStagger = +elem.getAttribute("data-effect-leave-stagger") || stagger
-        binding.effectEnterStagger = +elem.getAttribute("data-effect-enter-stagger") || stagger
-        binding.effectClass = elem.className || NaN
-    }
-}
-function upperFirstChar(str) {
-    return str.replace(/^[\S]/g, function (m) {
-        return m.toUpperCase()
-    })
-}
-//var effectBuffer = new Buffer()
-function Effect() {
-}//动画实例,做成类的形式,是为了共用所有原型方法
-
-Effect.prototype = {
-    contrustor: Effect,
-    enterClass: function () {
-        return getEffectClass(this, "enter")
-    },
-    leaveClass: function () {
-        return getEffectClass(this, "leave")
-    },
-    // 共享一个函数
-    actionFun: function (name, before, after) {
-        if (document.hidden) {
-            return
-        }
-        var me = this
-        var el = me.el
-        var isLeave = name === "leave"
-        name = isLeave ? "leave" : "enter"
-        var oppositeName = isLeave ? "enter" : "leave"
-        callEffectHook(me, "abort" + upperFirstChar(oppositeName))
-        callEffectHook(me, "before" + upperFirstChar(name))
-        if (!isLeave)
-            before(el) //这里可能做插入DOM树的操作,因此必须在修改类名前执行
-        var cssCallback = function (cancel) {
-            el.removeEventListener(me.cssEvent, me.cssCallback)
-            if (isLeave) {
-                before(el) //这里可能做移出DOM树操作,因此必须位于动画之后
-                avalon(el).removeClass(me.cssClass)
-            } else {
-                if (me.driver === "a") {
-                    avalon(el).removeClass(me.cssClass)
-                }
-            }
-            if (cancel !== true) {
-                callEffectHook(me, "after" + upperFirstChar(name))
-                after && after(el)
-            }
-            me.dispose()
-        }
-        if (me.useCss) {
-            if (me.cssCallback) { //如果leave动画还没有完成,立即完成
-                me.cssCallback(true)
-            }
-
-            me.cssClass = getEffectClass(me, name)
-            me.cssCallback = cssCallback
-
-            me.update = function () {
-                el.addEventListener(me.cssEvent, me.cssCallback)
-                if (!isLeave && me.driver === "t") {//transtion延迟触发
-                    avalon(el).removeClass(me.cssClass)
-                }
-            }
-            avalon(el).addClass(me.cssClass)//animation会立即触发
-
-            effectBuffer.render(true)
-            effectBuffer.queue.push(me)
-
-        } else {
-            callEffectHook(me, name, cssCallback)
-
-        }
-    },
-    enter: function (before, after) {
-        this.actionFun.apply(this, ["enter"].concat(avalon.slice(arguments)))
-
-    },
-    leave: function (before, after) {
-        this.actionFun.apply(this, ["leave"].concat(avalon.slice(arguments)))
-
-    },
-    dispose: function () {//销毁与回收到池子中
-        this.update = this.cssCallback = null
-        if (effectPool.unshift(this) > 100) {
-            effectPool.pop()
-        }
-    }
-
-
-}
-
-
-function getEffectClass(instance, type) {
-    var a = instance.callbacks[type + "Class"]
-    if (typeof a === "string")
-        return a
-    if (typeof a === "function")
-        return a()
-    return instance.name + "-" + type
-}
-
-
-function callEffectHook(effect, name, cb) {
-    var hook = effect.callbacks[name]
-    if (hook) {
-        hook.call(effect, effect.el, cb)
-    }
-}
 
 var applyEffect = function (el, dir/*[before, [after, [opts]]]*/) {
     var args = aslice.call(arguments, 0)
