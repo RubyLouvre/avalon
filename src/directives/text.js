f7c0597f322d4512a853a654770f738d060da0f5
RubyLouvre <1669866773@qq.com>
2016-05-30 13:06:10 +0800

    修正ms-text在某些情况下元素文本不显示的BUG

--- src/directives/text.js
+++ src/directives/text.js
@@ -11,15 +11,16 @@ avalon.directive('text', {
         var curValue = cur.props[name]
         var preValue = pre.props[name]
         cur.children = pre.children
-        cur.skipContent = true
         var dom = cur.dom = pre.dom
-        if (curValue !== preValue) {
-            if (!cur.children[0]) cur.children[0] = {type:"#text",nodeType:3}
+        cur.skipContent = true
+        if (curValue !== preValue || cur.children.length === 0) {
+            if (!cur.children[0])
+                cur.children[0] = {type: "#text", nodeType: 3}
             cur.children[0].nodeValue = curValue
             if (dom) {
                 this.update(dom, cur)
             } else {
-                update(cur, this.update, steps, 'text' )
+                update(cur, this.update, steps, 'text')
             }
         }
         pre.dom = null
