625ee9b9c0b29e933b8422571e0f5d0a4cad7323
RubyLouvre <1669866773@qq.com>
2016-06-02 03:11:31 +0800

    更好地抽取绑定属性

---
+++ src/strategy/parser/keyMap.js
@@ -0,0 +1,7 @@
+module.exports = avalon.oneObject("break,case,catch,continue,debugger,default,delete,do,else,false," +
+        "finally,for,function,if,in,instanceof,new,null,return,switch,this," +
+        "throw,true,try,typeof,var,void,while,with," + /* 关键字*/
+        "abstract,boolean,byte,char,class,const,double,enum,export,extends," +
+        "final,float,goto,implements,import,int,interface,long,native," +
+        "package,private,protected,public,short,static,super,synchronized," +
+        "throws,transient,volatile")
---
+++ src/strategy/parser/parseBindings2.js
@@ -0,0 +1,69 @@
+var directives = avalon.directives
+var rbinding = require('../../seed/regexp').binding
+var eventMap = avalon.oneObject('animationend,blur,change,input,click,dblclick,focus,keydown,keypress,keyup,mousedown,mouseenter,mouseleave,mousemove,mouseout,mouseover,mouseup,scan,scroll,submit')
+
+function parseBindings(cur, props) {
+    var bindings = []
+    var skip = 'ms-skip' in props
+    var uniq = {}
+    var isBreak
+    for (var i in props) {
+        var value = props[i], match
+
+        if (!skip && (match = i.match(rbinding))) {
+            var type = match[1]
+            var param = match[2] || ''
+            var name = i
+            if (eventMap[type]) {
+                var order = parseFloat(param) || 0
+                param = type
+                type = 'on'
+            }
+            name = 'ms-' + type + (param ? '-' + param : '')
+            if (i !== name) {
+                delete props[i]
+                props[name] = value
+            }
+            if (directives[type]) {
+
+                var binding = {
+                    type: type,
+                    param: param,
+                    name: name,
+                    expr: value,
+                    priority: directives[type].priority || type.charCodeAt(0) * 100
+                }
+                if (type === 'on') {
+                    order = order || 0
+                    binding.name += '-' + order
+                    binding.priority += param.charCodeAt(0) * 100 + order
+                }
+                if (!uniq[binding.name]) {
+                    uniq[binding.name] = 1
+                    bindings.push(binding)
+                }
+            }
+        } else {
+            cur.props[i] = props[i]
+        }
+    }
+
+
+    bindings.sort(byPriority)
+    var ret = []
+    for (var k = 0, el; el = bindings[k++]; ) {
+        var name = el.name
+        ret.name = name
+        ret.push(el)
+        if (name === 'important' || name === 'widget') {
+            break
+        }
+    }
+    return ret
+}
+
+function byPriority(a, b) {
+    return a.priority - b.priority
+}
+
+module.exports = parseBindings
