356e3b06f7fd5e20d031bbf97bcbfa033db0b4bb
RubyLouvre <1669866773@qq.com>
2016-06-20 14:44:44 +0800

    优化html 指令

--- src/directives/_update.js
+++ src/directives/_update.js
@@ -1,5 +1,4 @@
 module.exports = function (vdom, update, steps,  type, hookName) {
     var dom = vdom.dom
-    console.log(vdom, dom,'!!!!')
     update(vdom.dom, vdom, dom && dom.parentNode)
 }
--- src/directives/html.js
+++ src/directives/html.js
@@ -1,31 +1,43 @@
 var update = require('./_update')
 var parseView = require('../strategy/parser/parseView')
+var reconcile = require('../strategy/reconcile')
 
-avalon.htmlFactory = function (str, vm, local) {
-    var vtree = avalon.lexer(str + "")
-    var e = avalon.render(vtree)
-    return  e(vm, local)
-}
+
 
 avalon.directive('html', {
     parse: function (cur, pre, binding) {
+        
         if (!pre.isVoidTag) {
             //将渲染函数的某一部分存起来,渲在c方法中转换为函数
             cur[binding.name] = avalon.parseExpr(binding)
-            pre.children = []
-            cur.children = 'avalon.htmlFactory(' + avalon.parseExpr(binding) + ',__vmodel__,__local__)'
+            cur.vmodel = '__vmodel__'
+            cur.local = '__local__'
         }else{
             cur.children = '[]'
         }
     },
     diff: function (cur, pre, steps, name) {
-        var curValue = cur[name]
-        var preValue = pre[name]
-
-        if (curValue !== preValue) {
-            update(cur, this.update, steps, 'html')
+        var curValue = cur[name]+''
+        if (curValue !== pre[name]) {
+            pre[name] = curValue
+            var preTree = avalon.lexer(curValue)
+            avalon.speedUp(preTree)
+            pre.children = preTree
+            var render =  avalon.render(preTree)
+            pre.render = render
+            var curTree = render(cur.vmodel, cur.local)
+            cur.children = curTree
+            update(pre, this.update, steps, 'html')
+        }else{
+            var curTree = pre.render(cur.vmodel, cur.local)
+            cur.children = curTree
         }
     },
  
-    update: function(){}
+    update: function(dom, vdom){
+        avalon.clearHTML(dom)
+        var f = avalon.vdomAdaptor(vdom.children)
+        reconcile(f.children, vdom.children)
+        dom.appendChild(f)        
+    }
 })
