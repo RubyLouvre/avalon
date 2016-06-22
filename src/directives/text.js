6e52e368aa413be7ec02dec64758451e72237aa8
RubyLouvre <1669866773@qq.com>
2016-06-22 16:48:26 +0800

    fix ms-text BUG

--- src/directives/text.js
+++ src/directives/text.js
@@ -4,14 +4,14 @@ var update = require('./_update')
 avalon.directive('text', {
     parse: function (copy, src, binding) {
         src.children = [{nodeType:3,type:"#text",dynamic: true, nodeValue:"dynamic"}]
-        copy.children = '[]'
+        copy.children = '[{nodeType:3,type:"#text", nodeValue:"dynamic"}]'
         var val = rident.test(binding.expr) ? binding.expr : avalon.parseExpr(binding)
         copy[binding.name] = val
     },
     diff: function (copy, src, name) {
         var copyValue = copy[name]+''
         if (copyValue !== src[name] ) {
-            src[name] = copyValue
+            src[name] = copy.children[0].nodeValue = copyValue
             update(src, this.update)
         }
     },
