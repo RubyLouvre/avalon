7bb224e5fcd9ae7c58b573bb6ca811da4e100b56
RubyLouvre <cheng5128>
2016-07-13 01:38:39 +0800

    update

--- src/seed/lang.share.js
+++ src/seed/lang.share.js
@@ -32,7 +32,7 @@ avalon.shadowCopy(avalon, {
             return a === 'true'|| a == '1'
         }
     },
-    version: "2.10",
+    version: "2.11",
     slice: function (nodes, start, end) {
         return _slice.call(nodes, start, end)
     },
