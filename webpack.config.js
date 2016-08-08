f59120920c4b192f60313145e3a179db1aa60014
RubyLouvre <cheng5128>
2016-08-08 11:19:36 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,11 @@ function heredoc(fn) {
             }
 var feather = heredoc(function(){
     /*
-component/initjs中的protected变量更名为immunity,方便在严格模式下运行
-为伪事件对象过滤掉原生事件对象中的常量属性   
-修复class,hover,active指令互相干扰的BUG
-修复事件绑定中表达式太复杂,不会补上($event)的BUG
-当组件被移出DOM树并且没有被cached时,其虚拟DOM应该清空上面的事件
-重写for, widget指令,
+修正 ms-click 在 ms-if 下失效的问题 #1652
+修正 limitBy BUG
+修正 节点对齐算法 BUG
+优化 mediatorFactory
+修正 data-for-rendered BUG
      */
 })
 fs.writeFileSync('./src/seed/lang.share.js', text, 'utf8')
