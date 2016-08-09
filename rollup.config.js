1bd1aad823a4f1b49dc8c67c217d59d3011f8d3c
RubyLouvre <cheng5128>
2016-08-10 00:19:57 +0800

    使用rollup打包avalon!!! dist/avalon.rollup.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,12 @@
-var rollup = require("rollup").rollup
-var commonjs = require("rollup-plugin-commonjs")
-//var replace = require("rollup-plugin-replace")
-console.log("-----")
-rollup({
-  entry: "./src/avalon.js",
-  plugins: [ commonjs() ]
-}).then(function(bundle){
-
-
-      bundle.write({
-        dest: 'dist/index.js',
-        moduleName:"avalon",
-        format: 'umd'
-      });
-
-}).catch(function(e){
-  console.log(e)
-})
-//使用 rollup -c --input src/avalon
+import commonjs from 'rollup-plugin-commonjs';
+export default {
+    entry: 'src/avalon.js',
+    format: 'umd',
+    moduleName: 'avalon',
+    plugins: [
+        commonjs({
+            ignoreGlobal: true
+        })
+    ],
+    dest: 'dist/avalon.rollup.js'
+ };  //rollup -c 
