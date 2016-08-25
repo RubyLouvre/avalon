a4dd5a2ab55f9ea11d32fdd56bb05afaf4573175
RubyLouvre <cheng5128>
2016-08-25 16:43:06 +0800

    add

--- src/seed/cache.js
+++ src/seed/cache.js
@@ -23,34 +23,28 @@ function LRU(maxLength) {
 var p = LRU.prototype
 
 p.put = function (key, value) {
-    var removed
+    var entry = {
+        key: key,
+        value: value
+    }
+    this._keymap[key] = entry
+    if (this.tail) {
+        // 如果存在tail（缓存数组的长度不为0），将tail指向新的 entry
 
-    var entry = this.get(key, true)
-    // 如果不存在 key 这样属性的缓存对象，才能调用 put 方法
-    if (!entry) {
-        if (this.size === this.limit) {
-            // 如果缓存数组达到上限，则先删除 head 指向的缓存对象
-            removed = this.shift()
-        }
-        // 初始化赋值
-        entry = {
-            key: key
-        }
-        this._keymap[key] = entry
-        if (this.tail) {
-            // 如果存在tail（缓存数组的长度不为0），将tail指向新的 entry
-            this.tail.newer = entry
-            entry.older = this.tail
-        } else {
-            // 如果缓存数组的长度为0，将head指向新的entry
-            this.head = entry
-        }
-        this.tail = entry
+        this.tail.newer = entry
+        entry.older = this.tail
+    } else {
+        // 如果缓存数组的长度为0，将head指向新的entry
+        this.head = entry
+    }
+    this.tail = entry
+    // 如果缓存数组达到上限，则先删除 head 指向的缓存对象
+    if (this.size === this.limit) {
+        this.shift()
+    } else {
         this.size++
     }
-    entry.value = value
-
-    return removed
+    return value
 }
 
 p.shift = function () {
@@ -58,19 +52,21 @@ p.shift = function () {
     if (entry) {
         // 删除 head ，并改变指向
         this.head = this.head.newer
-        this.head.older = undefined
-        entry.newer = entry.older = undefined
         // 同步更新 _keymap 里面的属性值
-        this._keymap[entry.key] = undefined
+        this.head.older =
+                entry.newer =
+                entry.older =
+                this._keymap[entry.key] =
+                void 0
+        delete this._keymap[entry.key] //#1029
         // 同步更新 缓存数组的长度
         this.size--
     }
-    return entry
 }
 p.get = function (key) {
     var entry = this._keymap[key]
     // 如果查找不到含有`key`这个属性的缓存对象
-    if (entry === undefined)
+    if (entry === void 0)
         return
     // 如果查找到的缓存对象已经是 tail (最近使用过的)
     if (entry === this.tail) {
@@ -98,7 +94,7 @@ p.get = function (key) {
         entry.older.newer = entry.newer // C. --> E
     }
     // 处理所查找到的对象的 newer 以及 older 指向
-    entry.newer = undefined // D --x
+    entry.newer = void 0 // D --x
     // older指向之前使用过的变量，即D指向E
     entry.older = this.tail // D. --> E
     if (this.tail) {
