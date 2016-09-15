4ff0f156916d32eb701ef019dd6c6dab52b5ef31
RubyLouvre <cheng5128>
2016-09-15 23:53:30 +0800

    优化传参

--- src/directives/_update.js
+++ src/directives/_update.js
@@ -4,6 +4,6 @@ module.exports = function (vdom, update, hookName) {
         avalon.Array.ensure(vdom.afterChange, update)
     } else {
         var dom = vdom.dom
-        update(vdom.dom, vdom, dom && dom.parentNode)
+        update(dom, vdom, dom && dom.parentNode)
     }
 }
