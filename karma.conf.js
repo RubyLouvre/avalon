1d97a64648a9a8930b74d970925d89e7fbf88b73
RubyLouvre <cheng5128>
2016-11-30 22:34:12 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -103,13 +103,9 @@ module.exports = function(config) {
             },
             // Continuous Integration mode
             // if true, Karma captures browsers, runs the tests and exits
-            singleRun: false,
+            singleRun: true,
 
-            // Concurrency level
-            // how many browser should be started simultaneous
-            concurrency: Infinity,
-
-
+            
             // webpack: require('./webpack.config.js'),
 
             webpackServer: {
