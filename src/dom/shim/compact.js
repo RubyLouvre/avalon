8875307cbb66e8a38a739eaa5cebbe676ffc814d
RubyLouvre <cheng5128>
2016-12-07 02:07:58 +0800

    处理firefox的children

--- src/dom/shim/compact.js
+++ src/dom/shim/compact.js
@@ -1,10 +1,10 @@
-import { avalon, document, window, inBrowser, msie } from '../../seed/core'
+import { avalon, document, window, inBrowser, msie, root } from '../../seed/core'
 import { fixClone } from './fixClone'
 import { fixContains } from './fixContains'
 export { avalon }
 avalon.contains = fixContains
 
-avalon.cloneNode = function (a) {
+avalon.cloneNode = function(a) {
     return a.cloneNode(true)
 }
 
@@ -15,24 +15,37 @@ function shimHack() {
         avalon.cloneNode = fixClone
     }
     if (!document.contains) {
-        document.contains = function (b) {
+        document.contains = function(b) {
             return fixContains(document, b)
         }
     }
 
     if (window.Node && !document.createTextNode('x').contains) {
-        Node.prototype.contains = function (child) {//IE6-8没有Node对象
+        Node.prototype.contains = function(child) { //IE6-8没有Node对象
             return fixContains(this, child)
         }
     }
 
     //firefox 到11时才有outerHTML
-    if (window.HTMLElement && !avalon.root.outerHTML) {
-        HTMLElement.prototype.__defineGetter__('outerHTML', function () {
-            var div = document.createElement('div')
-            div.appendChild(this)
-            return div.innerHTML
-        })
+    if (window.HTMLElement) {
+        if (!root.outerHTML) {
+            HTMLElement.prototype.__defineGetter__('outerHTML', function() {
+                var div = document.createElement('div')
+                div.appendChild(this)
+                return div.innerHTML
+            })
+        }
+        if (!root.children) {
+            HTMLElement.prototype.__defineGetter__('children', function() {
+                var children = []
+                for (var i = 0, el; el = this.childNodes[i++];) {
+                    if (el.nodeType === 1) {
+                        children.push(el)
+                    }
+                }
+                return children
+            })
+        }
     }
 }
 
