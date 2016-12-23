a35e463b4682d033a28d39748bfda08b477bf0f1
RubyLouvre <1669866773@qq.com>
2016-12-23 21:48:42 +0800

    加大重连次数

--- karma.sauce.js
+++ karma.sauce.js
@@ -195,7 +195,7 @@ module.exports = function(config) {
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
-
+        retryLimit: 4,
         sauceLabs: {
             testName: 'get iphone badge',
             //recordScreenshots: false,
